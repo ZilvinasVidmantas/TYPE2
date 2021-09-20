35e1b6c88a67b14e92e09b82050efa7235ff90d1
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-09-20 10:56:44 +0300

    lesson 16: console methods

--- JavaScript/16 pamoka - kintamieji, spausdinimas, operatoriai ir duomenų tipai/variables-and-logging.js
+++ JavaScript/16 pamoka - kintamieji, spausdinimas, operatoriai ir duomenų tipai/variables-and-logging.js
@@ -4,6 +4,10 @@
 
 // Kintamojo deklaravimas - tai pasakymas vykdomam JS procesui/programai, jog bus naudojama atminties vieta, pasiekiama kintamojo pavadinimu
 
+// Kada naudoti 'const' o kada 'let'?
+// Atsakymas:
+//  Visada naudoti 'const', išskyrus tuos atvejus, kuomet PLANUOJATE keisti kintamojo reikšmę.
+
 // Tik deklarvimas
 let a; // vieno kintamojo deklaravimas
 console.log('a:', a); // spausdinimas
@@ -26,5 +30,61 @@ const d = 7; // deklaravimas, priskyrimas ir reikšmės pakeitimo uždraudimas
 // d = 5465; KLAIDA
 console.log('d:', d);
 
+// SPAUSINIMAS
+const students = [
+  { name: 'Serbentautas', surname: 'Bordiūras', age: 21, course: 3 },
+  { name: 'Stalčius', surname: 'Komoda', age: 20, course: 2 },
+  { name: 'Lempa', surname: 'Elektrytė', age: 21, course: 3 },
+  { name: 'Palangė', surname: 'Katytė', age: 19, course: 1 }
+];
+console.groupCollapsed('console.log');
+{
+  // console.log - atspausdinama vartotojui suprantamu būdu, galima atspausdinti daug dalykų, juos atskyrus kableliu
+  console.log(students);
+  console.log(a, b, c, d);
+  console.log('a:', a, 'b:', b, 'c:', c, 'd:', d);
+  console.log({ a, b, c, d });
+}
+console.groupEnd();
 
+console.groupCollapsed('console.dir');
+{
+  // console.dir - atspausdina kintamajį informatyviu būdu
+  const pageBody = document.body;
+  console.log(pageBody);
+  console.dir(pageBody);
+}
+console.groupEnd();
+
+console.groupCollapsed('console.table');
+{
+// console.table - atspausdina masyvo elementus eilutėmis
+console.log(7);
+console.table(7);
+console.log(students);
+console.table(students);
+}
+console.groupEnd();
+
+console.group('console.group');
+// console.group() + console.groupEnd(); - Patogus būdas atspausdinti grupę susijusių dalykų
+{
+  console.log('grupės turinys');
+  console.log('grupės turinys');
+  console.log('grupės turinys');
+  console.log('grupės turinys');
+  console.log('grupės turinys');
+}
+console.groupEnd();
+
+console.groupCollapsed('console.groupCollapsed');
+// console.groupCollapsed() + console.groupEnd(); - Patogus būdas atspausdinti grupę susijusių dalykų
+{
+  console.log('grupės turinys');
+  console.log('grupės turinys');
+  console.log('grupės turinys');
+  console.log('grupės turinys');
+  console.log('grupės turinys');
+}
+console.groupEnd();
 
