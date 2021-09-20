6555906736fae4a819ee38738b167b3767990694
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-09-20 11:43:40 +0300

    lesson 16: data types

--- JavaScript/Atskiros temos/data-types.js
+++ JavaScript/Atskiros temos/data-types.js
@@ -4,48 +4,51 @@
  * Primityvūs:
  *    Number: bet koks skaičius
  *    String: simbolių darinys rašomas tarp kabučių <'...'>, arba <"...">, arba <`...`>
- *    Loginis: true || false. Bulio algrebra grėstos reikšmės - tiesa, arba melas.
+ *    Boolean: true || false. Bulio algrebra grįstos reikšmės - tiesa, arba melas.
  *    undefined: reikšmės neaprašymas, tuštuma.
  *    null: pasakymas, kad reikšmės nėra
  * Nuorodos tipo:
- *    Object: rakto iš reikšių poros, su prototipu/šablonu
- *    Function - ↘
- *    Array    - → → → Bet kokie kiti nuorodos tipai yra Objekto papildymas/išplėtimas
- *    Date     - ↗
- *    ir daug kitų prototipų...
+ *    Object: rakto iš reikšių poros
+ *      Function - ↘
+ *      Array    - → → → Bet kokie kiti nuorodos tipai yra Objekto papildymas/išplėtimas
+ *      Date     - ↗
+ *      ir daug kitų prototipų...
 */
 
 console.group('Primityvių kintamųjų kopijavimas');
 {
-  const a = 1;
+  const a = 7;
   console.log('a:', a);
   let b = a;
   console.log('Padaryta a kintamojo kopija b:', b);
-  b = 7;
+  b = 'Pakeista';
   console.log('Pakeista b kintamojo reikšmė...');
   console.log('a:', a);
   console.log('b:', b);
   console.log('Išvada: Pagal kopijavimo pavyzdį, matome, jog kintamieji nėra susiję.');
   console.log('Pakeitus vieną - kitas nekinta.');
 }
-console.log('-------------------------');
-console.log();
+console.log('-------------------------\n');
 console.groupEnd();
 
 console.group('Nuorodos(objekto) tipo kintamųjų kopijavimas');
 {
   const a = [1, 2, 3];
   //         0  1  2
+  // a[0] -> 1
+  // a[1] -> 2
+  // a[2] -> 3
   console.log('a:', a);
   let b = a;
   console.log('Padaryta a kintamojo kopija b:', b);
-  b = 7;
+  b[0] = 'Pakeista';
+  b[1] = 'Pakeista';
+  b[2] = 'Pakeista';
   console.log('Pakeistos b masyvo reikšmės...');
   console.log('a:', a);
   console.log('b:', b);
-  console.log('Išvada: Pagal kopijavimo pavyzdį, matome, jog kintamieji nėra susiję.');
-  console.log('Pakeitus vieną - kitas nekinta.');
+  console.log('Išvada: Pagal kopijavimo pavyzdį, matome, jog kintamieji YRA susiję.');
+  console.log('Pakeitus vieną - keičiasi ir kitas.');
 }
-console.log('-------------------------');
-console.log();
+console.log('-------------------------\n');
 console.groupEnd();
