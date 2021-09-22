ae9767f63761ae661c1e99af3204ae149dcbf5fd
RokBan <rokas.banaitis@gmail.com>
2021-09-22 13:27:42 +0300

    lesson 17 - update on tasks

--- JavaScript/17 pamoka - objektai, masyvai, salyginiai sakiniai/praktika/uzduotys.js
+++ JavaScript/17 pamoka - objektai, masyvai, salyginiai sakiniai/praktika/uzduotys.js
@@ -1,18 +1,47 @@
-let amzius = prompt("Laba diena, koks jūsų amžius?");
-let teises;
+/*let amzius = prompt("Laba diena, koks jūsų amžius?");
 
 if(amzius >= 18){
   console.log("Jūs esate pilnametis!");
-  teises = prompt("Ar jau išsilaikęs mašinos teises?");
+  let teises = prompt("Ar jau išsilaikęs automobilio teises? (taip/ne)");
   if(teises == "Taip" || teises == "taip"){
-    console.log("yay");
+    console.log("Jūs jau turite automobilio teises.");
+    let automobilis = prompt("Ar turite automobilį? (taip/ne)");
+    if(automobilis == "Taip" || automobilis == "taip"){
+
+    } else if(automobilis == "Ne" || automobilis == "ne"){
+
+    } else {
+      console.log("Atsakymas turėjo būti 'taip' arba 'ne'. Prašome pateikti atsakymą per naują.");
+    }
+  } else if (teises == "Ne" || teises == "ne") {
+    console.log("Reikia išsilaikyti automobilio teises, kad galėtum jį vairuoti.");
   } else {
-    console.log("sad");
-  }
+    console.log("Atsakymas turėjo būti 'taip' arba 'ne'. Prašome pateikti atsakymą per naują.");
+  }*/
   /*console.log(10+amzius);
   console.log("Po 10 metų jums būtų " + ( 10 + amzius ) + " metų.");*/
-} else {
-  console.log("Jūs nesate pilnametis.");
+/*} else {
+  console.log("Jūs nesate pilnametis. Negalite vairuoti automobilio.");
+}*/
+
+let metuLaikas = prompt("Koks dabar metų laikas?");
+metuLaikas = metuLaikas.toLocaleLowerCase();
+switch (metuLaikas){
+  case "ruduo" :
+    console.log("Ruduo yra gražus metų laikas. Visi medžiai meta savo spalvotus lapus.");
+    break;
+  case "žiema" :
+    console.log(metuLaikas + " šalta.");
+    break;
+  case "vasara" :
+    console.log(metuLaikas + " karšta.");
+    break;
+  case "pavasaris" :
+    console.log(metuLaikas + " viskas žydi.");
+    break;
+  default:
+    console.log("error");
 }
 
-console.log(teises);
+
+
