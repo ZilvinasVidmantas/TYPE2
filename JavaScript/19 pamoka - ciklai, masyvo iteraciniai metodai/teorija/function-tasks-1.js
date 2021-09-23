2f6c21c828ff50a3e513f163f73a95aca788d512
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-09-23 11:59:10 +0300

    lesson 19: task sufflers

---
+++ JavaScript/19 pamoka - ciklai, masyvo iteraciniai metodai/teorija/function-tasks-1.js
@@ -0,0 +1,96 @@
+console.groupCollapsed('1. Parašykite funkciją, kuri grąžina pirmą masyvo elementą.');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('2. Parašykite funkciją, kuri pašalina pirmą masyvo elementą ir jį grąžina.');
+{
+  // ...sprendimas ir spausdinimas
+  // google: mdn remove first array element
+}
+console.groupEnd();
+
+console.groupCollapsed('3. Parašykite funkciją, kuri grąžina paskutinį masyvo elementą.');
+{
+  // ...sprendimas ir spausdinimas
+  // google: js last array element
+}
+console.groupEnd();
+
+console.groupCollapsed('4. Parašykite funkciją, kuri pašalina paskutinį masyvo elementą ir jį grąžina.');
+{
+  // ...sprendimas ir spausdinimas
+  // google: js last array element
+}
+console.groupEnd();
+
+console.groupCollapsed('5. Parašykite funkciją, kuri grąžina elementų kiekį masyve');
+{
+  // ...sprendimas ir spausdinimas
+  // google: js element count in array
+}
+console.groupEnd();
+
+console.groupCollapsed('6. Parašykite funkciją, kuri atspausdina paskutinio masyvo elemento indeksą');
+{
+  // ...sprendimas ir spausdinimas
+  // google: js last array element index
+}
+console.groupEnd();
+
+console.groupCollapsed('7. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus eilutėmis');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('8. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes eilutėmis');
+{
+  // ...sprendimas ir spausdinimas
+}
+console.groupEnd();
+
+console.groupCollapsed('9. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras eilutėmis, tokiu formatu:  ');
+// [0] => reikšmė
+// [1] => reikšmė
+// [2] => reikšmė
+{
+  // ... sprendimas ir spausdinimas
+  // google: js string literal
+}
+console.groupEnd();
+
+console.groupCollapsed('10. Parašykite funkciją, kuri atspausdina masyvo elementus atbuline tvarka eilutėmis, iš galo.');
+{
+  // ...sprendimas ir spausdinimas
+  // google: js iterate through array reverse
+}
+console.groupEnd();
+
+console.groupCollapsed('11. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus vienoje eilutėje: 0 1 2 3 ...');
+{
+  // ...sprendimas ir spausdinimas
+  // google: js string literal
+  // ARBA
+  // google: js join array elements with separator
+}
+console.groupEnd();
+
+console.groupCollapsed('12. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes vienoje eilutėje: -111 2 -9 48');
+{
+  // ...sprendimas ir spausdinimas
+  // google: js string literal
+  // ARBA
+  // google: js join array elements with separator
+}
+console.groupEnd();
+
+console.groupCollapsed('13. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras vienoje eilutėje, tokiu formatu:');
+//  [0]=>17 [1]=>8 [2]=>88 ...
+{
+  // google: js string literal
+  // IR
+  // google: js join array elements with separator
+}
+console.groupEnd();
--- JavaScript/19 pamoka - ciklai, masyvo iteraciniai metodai/teorija/klausimu-atsakymai.js
+++ JavaScript/19 pamoka - ciklai, masyvo iteraciniai metodai/teorija/klausimu-atsakymai.js
@@ -20,3 +20,31 @@ console.group('Sukurkite funkcija, kuri atspausdintų skaičių seką nuo pirmoj
   printRange(-1, -1);
 }
 console.groupEnd();
+
+console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
+{
+  function byPriceASC(drink1, drink2) {
+    return drink1.price - drink2.price;
+  }
+  function byPriceDESC(drink1, drink2) {
+    return -1 * byPriceASC(drink1, drink2);
+  }
+
+  const drinks = [
+    { name: "lemonade", price: 50 },
+    { name: "cola", price: 10 },
+    { name: "fanta", price: 90 },
+    { name: "tea", price: 100 },
+    { name: "coffe", price: 1110 },
+    { name: "lime", price: 5 }
+  ];
+  const drinksByPriceASC = drinks.slice().sort(byPriceASC);
+  const drinksByPriceDESC = drinks.slice().sort(byPriceDESC);
+
+  console.log({
+    drinks,
+    drinksByPriceASC,
+    drinksByPriceDESC
+  });
+}
+console.groupEnd();
