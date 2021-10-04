24b6d0bc2601af9cf01fe01bdbbd6ef52d0e8d44
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-04 12:02:08 +0300

    lesson 26: filter function implementation

--- JavaScript/26 pamoka - aukštesnio lygio funkcijos/hofArrayIterativeMethodsCreation.js
+++ JavaScript/26 pamoka - aukštesnio lygio funkcijos/hofArrayIterativeMethodsCreation.js
@@ -1,15 +1,25 @@
 const arr = [1, 2, 3, 4, 5, 6];
 
-const forEach = (arr, callback) => {
+const forEach = (arr, doStuff) => {
   for (let i = 0; i < arr.length; i++) {
-    callback(arr[i], i, arr);
+    doStuff(arr[i], i, arr);
   }
 }
 
-const map = (array, callback) => {
+const map = (array, changeElement) => {
   const newArray = [];
   for (let i = 0; i < array.length; i++) {
-    newArray.push(callback(array[i], i, array));
+    newArray.push(changeElement(array[i], i, array));
+  }
+  return newArray;
+}
+
+const filter = (array, isTruthy) => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if(isTruthy(array[i], i, array)){
+      newArray.push(array[i]);
+    }
   }
   return newArray;
 }
@@ -26,6 +36,16 @@ console.group('map panaudojimo pavyzdys');
     arr,
     numbersDoubled: map(arr, x => x * 2),
     numbersSquared: map(arr, x => x ** 2)
+  });
+}
+console.groupEnd();
+
+console.group('filter panaudojimo pavyzdys');
+{
+  console.log({
+    arr,
+    numbersEqual: map(arr, x => x % 2 === 0),
+    numbersOdd: map(arr, x => x % 2 === 1)
   });
 }
 console.groupEnd();
