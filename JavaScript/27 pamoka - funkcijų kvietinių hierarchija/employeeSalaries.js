9045016f00ae43a78b6b443669748f50c7480af4
Žilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-05 12:05:46 +0300

    lesson 27: graphical representation start

--- JavaScript/27 pamoka - funkcijų kvietinių hierarchija/employeeSalaries.js
+++ JavaScript/27 pamoka - funkcijų kvietinių hierarchija/employeeSalaries.js
@@ -52,3 +52,56 @@ const company = {
 }
 
 // Suskaičiuoti, kokią sumą reikia išmokėti darbuotojams
+
+// Sprendimas A
+//    1. Atrenkami visi darbuotojai pagal individualią veiklą, ir suskaičiuojami jų atlyginimai
+//    2. Atrenkami visi darbuotojai pagal darbo sutartį, ir suskaičiuojami jų atlyginimai
+//    3. Grąžinamas [1.] + [2.]
+const contractTypeSalaryFunctions = [
+  { type: 'Individual activity', salaryFunction: (employee) => employee.hourPay * employee.hoursWorked },
+  { type: 'Contract', salaryFunction: (employee) => employee.salary },
+];
+const result1 = contractTypeSalaryFunctions.reduce((allEmployeesSalarySum, { type, salaryFunction }) => {
+  const employeesByContractType = company.departaments.reduce((employees, departament) => [
+    ...employees,
+    ...departament.employees.filter(x => x.contractType === type)
+  ], []);
+  const employeeSalariesByContractType = employeesByContractType.reduce((sum, employee) => sum + salaryFunction(employee), 0);
+  return allEmployeesSalarySum + employeeSalariesByContractType;
+}, 0)
+
+// Sprendimas B
+//    1. Skaičiuojame kiekvieno departamento darbuotojų atlyginimus, ir sumuojame kiekvieno departamento darbuotojų atlyginimo sumas
+
+const result2 = company.departaments.reduce((allEmployeesSalarySum, { employees }) => {
+  const departamentSalaries = employees.reduce((sum, employee) => {
+    let salary;
+    if (employee.contractType === 'Individual activity') {
+      salary = employee.hourPay * employee.hoursWorked;
+    } else {
+      salary = employee.salary;
+    }
+    return sum + salary;
+  }, 0);
+  return allEmployeesSalarySum + departamentSalaries
+}, 0);
+
+// Sprendimas C
+//    1. Visus darbuotojus sudedame į vieną masyvą ir tuomet sumuojame darbuotojų atlyginimus
+const result3 = company.departaments
+  .reduce((allWorkers, { employees }) => [...allWorkers, ...employees], [])
+  .reduce((sum, employee) => {
+    let salary;
+    if (employee.contractType === 'Individual activity') {
+      salary = employee.hourPay * employee.hoursWorked;
+    } else {
+      salary = employee.salary;
+    }
+    return sum + salary;
+  }, 0)
+
+console.log({
+  result1,
+  result2,
+  result3
+});
