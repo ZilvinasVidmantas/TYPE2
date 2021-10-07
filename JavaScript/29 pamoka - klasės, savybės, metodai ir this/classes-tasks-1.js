4398c12d5b7b342c226b1efdc31eed6f943b4e17
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-07 11:09:13 +0300

    lesson 29: classes-tasks-1.js

--- JavaScript/29 pamoka - klasės, savybės, metodai ir this/classes-tasks-1.js
+++ JavaScript/29 pamoka - klasės, savybės, metodai ir this/classes-tasks-1.js
@@ -3,7 +3,7 @@
   - sukurti konstruktorių, su 3 argumentais:
     - marke, modelis, kaina
 
-  - konstruktoriuje aprašyti, jog perduotus argumentus išsaugotume formuojame objekte savybėmis:
+  - konstruktoriuje aprašyti, jog perduotus argumentus išsaugotume formuojamame objekte - savybėmis:
     - this.brand
     - this.model
     - this.price
@@ -35,5 +35,62 @@
   ???
 
 6. Po padidinimo, vėl atsispausdinkite objektus ir pažiūrėkite pakitusias Automobilis objektų savybes.
-
 */
+
+class Automobilis {
+  constructor(brand, model, price) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+}
+
+const car1 = new Automobilis('BMW', 'X5', 8000);
+const car2 = new Automobilis('Volkswagen', 'Passat', 3000);
+const car3 = new Automobilis('Bugatti', 'Veyron', 500000);
+
+console.log({
+  car1,
+  car2,
+  car3
+});
+
+console.group('modeliai:');
+{
+  console.log(car1.model);
+  console.log(car2.model);
+  console.log(car3.model);
+}
+console.groupEnd();
+
+console.group('markės:');
+{
+  console.log(car1.brand);
+  console.log(car2.brand);
+  console.log(car3.brand);
+}
+console.groupEnd();
+
+console.group('kainos:');
+{
+  console.log(car1.price);
+  console.log(car2.price);
+  console.log(car3.price);
+}
+console.groupEnd();
+
+console.group('5. Padidinkite Automobilis objektų kainas šimtu.');
+{
+  car1.price = car1.price + 100;
+  car2.price = car2.price + 100;
+  car3.price += 100;
+}
+console.groupEnd();
+
+console.group('6. Po padidinimo, vėl atsispausdinkite objektus ir pažiūrėkite pakitusias Automobilis objektų savybes.');
+{
+  console.log(car1.price);
+  console.log(car2.price);
+  console.log(car3.price);
+}
+console.groupEnd();
