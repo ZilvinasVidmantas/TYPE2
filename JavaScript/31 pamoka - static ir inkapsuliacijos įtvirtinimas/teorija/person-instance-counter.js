1f37fcb8cae3ce3b6451c30de2cbb5f1f25ce163
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-11 12:03:37 +0300

    lesson 31: person instance counter done

--- JavaScript/31 pamoka - static ir inkapsuliacijos įtvirtinimas/teorija/person-instance-counter.js
+++ JavaScript/31 pamoka - static ir inkapsuliacijos įtvirtinimas/teorija/person-instance-counter.js
@@ -1,5 +1,47 @@
 class Person {
-  static count = 0;
+  static #locales = {
+    general: {
+      active: true,
+      formatDate: (yyyy, mm, dd) => `${yyyy}-${mm}-${dd}`
+    },
+    usa: {
+      active: false,
+      formatDate: (yyyy, mm, dd) => `${mm}-${dd}-${yyyy}`
+    },
+    chinese: {
+      active: false,
+      formatDate: (yyyy, mm, dd) => `${dd}-${mm}-${yyyy}`
+    }
+  };
+  static #count = 0;
+  static centuriesByIdFirstNumber = {
+    1: '18',
+    2: '18',
+    3: '19',
+    4: '19',
+    5: '20',
+    6: '20'
+  };
+
+  static get count() {
+    return Person.#count;
+  }
+
+  static set locale(newLocale) {
+    const localeNames = Object.keys(Person.#locales);
+    if (!localeNames.includes(newLocale)) throw new TypeError(`Nėra tokios lokalės: '${newLocale}'. Galimos lokalės:\n\t${localeNames.join('\n\t')}`);
+    for (const localeName in Person.#locales) {
+      Person.#locales[localeName].active = false;
+    }
+    Person.#locales[newLocale].active = true;
+  }
+
+  static get formatDate() {
+    const localeObjectArray = Object.values(Person.#locales);
+    const currentLocaleObject = localeObjectArray.find(locale => locale.active);
+    const formatDateFunction = currentLocaleObject.formatDate;
+    return formatDateFunction;
+  }
 
   #id;
   #name;
@@ -9,6 +51,7 @@ class Person {
     this.id = id;
     this.name = name;
     this.surname = surname;
+    Person.#count++;
   }
 
   set id(newId) {
@@ -29,7 +72,7 @@ class Person {
     this.#name = newName[0].toUpperCase() + newName.slice(1);
   }
 
-  get name(){
+  get name() {
     return this.#name;
   }
 
@@ -39,7 +82,7 @@ class Person {
     this.#surname = newSurname[0].toUpperCase() + newSurname.slice(1);
   }
 
-  get surname(){
+  get surname() {
     return this.#surname;
   }
 
@@ -47,12 +90,25 @@ class Person {
     return this.#name + ' ' + this.#surname;
   }
 
-  get birthDate(){
-    // kodas čia
+  get birthDate() {
+    const yyyy = Person.centuriesByIdFirstNumber[this.#id[0]] + this.#id.slice(1, 3);
+    const mm = this.#id.slice(3, 5);
+    const dd = this.#id.slice(5, 7);
+    return Person.formatDate(yyyy, mm, dd);
   }
 
-  get sex(){
-    // kodas čia
+  get sex() {
+    return Number(this.#id[0]) % 2 === 1 ? 'vyras' : 'moteris';
+  }
+
+  get age() {
+    const dateNow = new Date();
+    const dateBirth = new Date(this.birthDate);
+    const yearNow = dateNow.getFullYear();
+    const yearBirth = dateBirth.getFullYear();
+    const age = yearNow - yearBirth;
+    return age;
+    return new Date().getFullYear() - new Date(this.birthDate).getFullYear();
   }
 }
 
@@ -62,14 +118,53 @@ const people = [
   new Person('39101035555', 'Saulėnas', 'Akinys'),
   new Person(49001045555, 'Lempa', 'Stalinskaitė')
 ];
+console.groupCollapsed('Privačios statinės savybės panaudojimas');
+{
+  console.log('Person objektų kiekis:', Person.count);
+  new Person('39301015555', 'KiBIras', 'Kiauraitis');
+  Person.count = 77;
+  console.log('Person objektų kiekis:', Person.count);
+}
+console.groupEnd();
 
-/*
-  https://lt.wikipedia.org/wiki/Asmens_kodas
-  Parašyti get'erį, kuris grąžintų gimimo datą, tokiu formatu:
-    yyyy-mm-dd
-  Parašyti get'erį, kuris grąžintų lytį: 'vyras' | 'moteris'
-*/
-console.log('gimimo datos');
-people.forEach(p => console.log(p.birthDate));
-console.log('lytys');
-people.forEach(p => console.log(p.sex));
+console.groupCollapsed('Dinamiškas statinis seteris su validacija ir objekto metodo panaudojimas pagal statinio objekto būseną');
+{
+  console.group('gimimo datos general');
+  {
+
+    people.forEach(p => console.log(p.fullname, '-', p.birthDate));
+    console.group('gimimo datos usa');
+  }
+  console.groupEnd();
+  {
+    Person.locale = 'usa';
+    people.forEach(p => console.log(p.fullname, '-', p.birthDate));
+  }
+  console.groupEnd();
+  console.group('gimimo datos chinese');
+  {
+    Person.locale = 'chinese';
+    people.forEach(p => console.log(p.fullname, '-', p.birthDate));
+  }
+  console.groupEnd();
+}
+console.groupEnd();
+
+
+const obj = {
+  x: 1,
+  y: [1, 2, 3],
+  z: -7,
+}
+
+for (const propertyName in obj) {
+  if (obj[propertyName] instanceof Array) {
+    console.group(propertyName);
+    {
+      obj[propertyName].forEach((x, i) => console.log(`${propertyName}[${i}] => ${x}`));
+    }
+    console.groupEnd()
+  } else {
+    console.log(propertyName, obj[propertyName]);
+  }
+}
