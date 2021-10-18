f0a2700860e46f7cdb090a39ee484176d1de9fb5
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-18 12:05:41 +0300

    lesson 36: element search

--- JavaScript - DOM/36 pamoka - įvadas/main.js
+++ JavaScript - DOM/36 pamoka - įvadas/main.js
@@ -1,10 +1,13 @@
 //  By attribute id=""
 const main = document.querySelector('#main');
 //  By attribute class=""
-const h1 = document.querySelector('.h1');
+const h1 = main.querySelector('.h1');
 //  By tag name <p>
-const p = document.querySelector('p');
+const mainParagraph = main.querySelector('#mainParagraph');
+// Select many by criteria
+const mainListItems = document.querySelectorAll('.js-mainListItem');
 
 console.log(main);
 console.log(h1);
-console.log(p);
+console.log(mainParagraph);
+console.log(mainListItems);
