8eaf8a2b60d066dad5c88449c3ead39fccd9605f
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-18 11:27:01 +0300

    lesson 36: query slector example

---
+++ JavaScript - DOM/36 pamoka - įvadas/main.js
@@ -0,0 +1,10 @@
+//  By attribute id=""
+const main = document.querySelector('#main');
+//  By attribute class=""
+const h1 = document.querySelector('.h1');
+//  By tag name <p>
+const p = document.querySelector('p');
+
+console.log(main);
+console.log(h1);
+console.log(p);
