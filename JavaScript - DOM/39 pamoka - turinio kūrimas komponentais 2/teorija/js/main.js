6466bd274aca1141a95dd6b3bd14d798e2308a16
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-21 10:46:16 +0300

    lesson 39: added .gitignore into root directory

--- JavaScript - DOM/39 pamoka - turinio kūrimas komponentais 2/teorija/js/main.js
+++ JavaScript - DOM/39 pamoka - turinio kūrimas komponentais 2/teorija/js/main.js
@@ -2,24 +2,18 @@ const root = document.querySelector('#root');
 
 const singleSectionData = sectionData[0];
 
-// const changePropComponent2ComponentName = ({ component, ...props }) => ({
-//   ...props,
-//   componentName: component,
-// })
+const changePropComponent2ComponentName = ({ component, ...props }) => ({
+  ...props,
+  componentName: component,
+})
 
-// const sectionComponent1 = new Section({
-//   title: singleSectionData.title,
-//   children: singleSectionData.children.map(changePropComponent2ComponentName),
-//   subSections: singleSectionData.subSections.map(({ children, ...subSection }) => ({
-//     ...subSection,
-//     children: children.map(changePropComponent2ComponentName)
-//   })),
-// });
+const sectionComponent1 = new Section({
+  title: singleSectionData.title,
+  children: singleSectionData.children.map(changePropComponent2ComponentName),
+  subSections: singleSectionData.subSections.map(({ children, ...subSection }) => ({
+    ...subSection,
+    children: children.map(changePropComponent2ComponentName)
+  })),
+});
 
-// root.appendChild(sectionComponent1.htmlElement);
-
-debugger;
-const sectionLVL1 = new Section({ title: 'Pirmo lygio sekcija' });
-const sectionLVL2 = new Section({ title: 'Antro lygio sekcija', level: 2 });
-
-root.append(sectionLVL1.htmlElement, sectionLVL2.htmlElement);
+root.appendChild(sectionComponent1.htmlElement);
