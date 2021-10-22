b56b9b4e632e0c4d3e7c70d67fd1ab764cd3dee0
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-22 11:23:22 +0300

    lesson 40: component creation practice

--- JavaScript - DOM/40 pamoka - turinio kūrimas komponentais 3/teorija/js/main.js
+++ JavaScript - DOM/40 pamoka - turinio kūrimas komponentais 3/teorija/js/main.js
@@ -1,32 +1,13 @@
 const root = document.querySelector('#root');
-/*
-  Perrašyti šį kodą, jog:
-    1. Veiksmai būtų atliekami su kiekvienu <sectionData> masyvo elementu ( o ne į'hard-code'inta kaip 24-25 eil.)
-      Keičiant kiekvieną duomenų objektą <sectionData> masyve, reikia įvertinti jog, jų savybėje <subSections>, taip pat gali reikėti 
-      padaryti pakeitimus taip, kaip skiriasi seckcijų duomenų formavimas <section1Props> ir <section2Props> atvejais.
-
-      Papildomai, kol laukiate kitų:
-        Šiuo metu nėra, pavyzdžio, kaip reikėtų elgtis, jeigu ir sekcijos <subSections> savybės <subSections>  (ir t.t.) savybėje būtų kitų sekcijų.
-        Peržiūrėkite video:
-          https://www.youtube.com/watch?v=LteNqj4DFD8&ab_channel=DevSage
-        Paskaitykite failą:
-          * TYPE2\JavaScript\Atskiros temos kas nori anksciau\rekursija.js
-        Peržiūrėkite paveikslėlius:
-          * TYPE2\JavaScript\vaizdiniai pavyzdžiai\Rekursija fibonacci.png
-          * TYPE2\JavaScript\vaizdiniai pavyzdžiai\Rekursija.png
-        Pabandykite savarankiškai išspręsti kelis uždavinius:
-          https://www.w3resource.com/javascript-exercises/javascript-recursion-functions-exercises.php
-      
-  */
 
 const changeChildComponentProps = ({ component, ...props }) => ({
   ...props,
   componentName: component,
 });
 
-const formatSectionsData = sectionArr => {
+const formatSectionsData = sectionDataArr => {
   // Grąžiname naujai suformuotą sekcijų duomenų masyvą, pakeisdami kiekvieną sekcijos duomenų objektą pagal funkciją 30-41
-  return sectionArr.map(
+  return sectionDataArr.map(
     ({ subSections, children, ...rest }) => {
       // į rezultato objektą įrašome, visas objekto savybes, kurių nereikia keisti arba tikrinti
       const result = { ...rest };
@@ -42,9 +23,42 @@ const formatSectionsData = sectionArr => {
   );
 }
 
-debugger;
 const sectionDataFormatted = formatSectionsData(sectionData);
 const sectionComponents = sectionDataFormatted.map(data => new Section(data));
-const sectionHtmlElements = sectionDataFormatted.map(component => component.htmlElement);
+const sectionHtmlElements = sectionComponents.map(component => component.htmlElement);
 
-root.append(...sectionHtmlElements);
+// root.append(...sectionHtmlElements);
+
+/* 
+  ------------------ 1 dalis - ContactComponent kūrimas ---------------------------
+  1. Peržiūrėkite nuotrauką 'task.png'
+  2. Suformuokite vieno komponento objektą <actorData>
+  3. Sukurkite savo komponentą 'ContactComponent', kuris panaudotų [2.] objekto duomenis suformuoti(render) vaizdą pagal 'task.png'
+    type ContactComponentProps = {
+      fullname: string,
+      imgSrc: string,
+      role: string,
+    }
+  4. prijungti šio [3.] komponento vaizdą prie <root>
+
+  ------------------ 2 dalis - ContactContainerComponent kūrimas ---------------------------
+  1. Peržiūrėkite nuotrauką 'task.png'
+  2. Suformuokite masyvą <actorDataArr>, kuriame būtų objektai, pagal 1 dalies [2.] užduotį
+  3. Sukurkite savo komponentą 'ContactContainerComponent', kuris panaudotų [2.]<actorDataArr> duomenis suformuoti(render) išdėstymą pagal 'task.png'
+      type ContactContainerComponentProps = Array<ContactComponentProps>
+  4. prijungti šio [3.] komponento vaizdą prie <root>
+*/
+
+/*
+  Papildomai, kol laukiate kitų:
+    Šiuo metu nėra, pavyzdžio, kaip reikėtų elgtis, jeigu ir sekcijos <subSections> savybės <subSections>  (ir t.t.) savybėje būtų kitų sekcijų.
+    Peržiūrėkite video:
+      https://www.youtube.com/watch?v=LteNqj4DFD8&ab_channel=DevSage
+    Paskaitykite failą:
+      * TYPE2\JavaScript\Atskiros temos kas nori anksciau\rekursija.js
+    Peržiūrėkite paveikslėlius:
+      * TYPE2\JavaScript\vaizdiniai pavyzdžiai\Rekursija fibonacci.png
+      * TYPE2\JavaScript\vaizdiniai pavyzdžiai\Rekursija.png
+    Pabandykite savarankiškai išspręsti kelis uždavinius:
+      https://www.w3resource.com/javascript-exercises/javascript-recursion-functions-exercises.php
+*/
