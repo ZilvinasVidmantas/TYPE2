ff83324ba1abbbdf498eeb3d91ba95e3936aab7e
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-25 10:13:39 +0300

    lesson 41: table data created

--- JavaScript - DOM/41 pamoka - turinio komponentų atnaujinimas/js/components/table-components.js
+++ JavaScript - DOM/41 pamoka - turinio komponentų atnaujinimas/js/components/table-components.js
@@ -6,6 +6,25 @@ class TableComponent {
   }
 
   render = () => {
-    this.htmlElement.className = 'table';
+    const colNames = this.props.colNames;
+    const data = this.props.data;
+
+    const tableHeaders = colNames.map(colName => `<th>${colName}</th>`).join('');
+
+    const tableRows = data
+      .map(rowData => {
+        const rowCols = rowData.map(text => `<td>${text}</td>`).join('');
+        return `<tr>${rowCols}</tr>`;
+      })
+      .join('');
+
+    this.htmlElement.className = 'table table-striped';
+    this.htmlElement.innerHTML = `
+    <thead class="thead-dark text-white">
+      <tr class="bg-dark">${tableHeaders}</tr>
+    </thead>
+    <tbody>${tableRows}</tbody>`;
   }
 }
+
+// 10:25
--- JavaScript - DOM/41 pamoka - turinio komponentų atnaujinimas/js/main.js
+++ JavaScript - DOM/41 pamoka - turinio komponentų atnaujinimas/js/main.js
@@ -1,10 +1,14 @@
 const rootElement = document.querySelector('#root');
 
 const tableComponent = new TableComponent({
-  colNames: ['Nuotrauka', 'El. paštas', 'Rolė']
+  colNames: ['Nuotrauka', 'El. paštas', 'Rolė'],
+  data: [
+    ['nuotrauka1', 'elementas1', 'rolė1'],
+    ['nuotrauka2', 'elementas2', 'rolė2'],
+    ['nuotrauka3', 'elementas3', 'rolė3'],
+    ['nuotrauka4', 'elementas4', 'rolė4'],
+    ['nuotrauka5', 'elementas5', 'rolė5'],
+  ]
 });
 
-console.log(tableComponent);
-
 rootElement.appendChild(tableComponent.htmlElement);
-// 9:22
