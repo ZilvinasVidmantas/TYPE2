d5454c5437ae3ec4c2c443ef8f06c2a3733f54a1
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-25 11:20:43 +0300

    lesson 41: created row Elements

--- JavaScript - DOM/41 pamoka - turinio komponentų atnaujinimas/js/components/table-components.js
+++ JavaScript - DOM/41 pamoka - turinio komponentų atnaujinimas/js/components/table-components.js
@@ -5,24 +5,51 @@ class TableComponent {
     this.render();
   }
 
+  /**
+   * Sukuria vienos eilutės html elementą
+   * 
+   * @param {Object} rowData vienos eilutės duomenys
+    * 
+   * @returns {HTMLElement} vienos eilutės html elementas
+   */
+
+  createRowElement = rowData => {
+    const rowCols = rowData.map(text => `<td>${text}</td>`).join('');
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <tr>
+      ${rowCols}
+      <td><button class="btn btn-sm btn-danger">✕</button></td>
+    </tr>`;
+
+    const btnDelete = row.querySelector('.btn-danger');
+    btnDelete.addEventListener('click', () => 'Aš tave išktrinsu, eiliau.');
+
+    return row;
+  }
+
+  /**
+   * Sukuria eilučių masyvą, pagal šio objekto props.data duomenis
+   * 
+   * @return {Array} eilučių html eilučių masyvas
+   */
+  createRows = () => this.props.data.map(this.createRowElement);
+
   render = () => {
     const colNames = this.props.colNames;
-    const data = this.props.data;
 
     const tableHeaders = colNames.map(colName => `<th>${colName}</th>`).join('');
-
-    const tableRows = data
-      .map(rowData => {
-        const rowCols = rowData.map(text => `<td>${text}</td>`).join('');
-        return `<tr>${rowCols}</tr>`;
-      })
-      .join('');
+    const rows = this.createRows();
+    console.log(rows);
 
     this.htmlElement.className = 'table table-striped';
     this.htmlElement.innerHTML = `
     <thead class="thead-dark text-white">
       <tr class="bg-dark">${tableHeaders}</tr>
     </thead>
-    <tbody>${tableRows}</tbody>`;
+    <tbody></tbody>`;
+
+    const tbody = this.htmlElement.querySelector('tbody');
+    tbody.append(...rows);
   }
 }
--- JavaScript - DOM/41 pamoka - turinio komponentų atnaujinimas/js/main.js
+++ JavaScript - DOM/41 pamoka - turinio komponentų atnaujinimas/js/main.js
@@ -1,22 +1,21 @@
 const rootElement = document.querySelector('#root');
 
-const formatedUserData = userDataArr.reduce((result, userData) =>{
 
-  result.push('Aš esu vienos eilutės duomenys');
+
+const formatedUserData = userDataArr.reduce((result, { imgSrc, email, role }) => {
+  result.push([imgSrc, email, role]);
   return result;
 }, []);
-console.log(formatedUserData);
+/* ES6  sintaksės sutrumpinimas
+  const formatedUserData = userDataArr.reduce((result, { imgSrc, email, role }) => [
+    ...result,
+    [imgSrc, email, role]
+  ], []);
+*/
 
 
 const tableComponent = new TableComponent({
   colNames: ['Nuotrauka', 'El. paštas', 'Rolė'],
-  // data: [
-  //   ['nuotrauka1', 'elementas1', 'rolė1'],
-  //   ['nuotrauka2', 'elementas2', 'rolė2'],
-  //   ['nuotrauka3', 'elementas3', 'rolė3'],
-  //   ['nuotrauka4', 'elementas4', 'rolė4'],
-  //   ['nuotrauka5', 'elementas5', 'rolė5'],
-  // ],
   data: formatedUserData
 });
 
