fae28888cfb1928d611f159281120fd25daea224
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-26 08:27:05 +0300

    lesson 42: homework

---
+++ JavaScript - DOM/42 pamoka - komponentų atnaujinimas/teorija/js/data/dog-data.js
@@ -0,0 +1,23 @@
+const dogDataArr = [
+  {
+    id: '1',
+    name: 'Barkey',
+    breed: 'Dalmantine',
+    imgSrc: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234026/Dalmatian-On-White-01.jpg'
+  }, {
+    id: '2',
+    name: 'Rex',
+    breed: 'German Shepard',
+    imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg'
+  }, {
+    id: '3',
+    name: 'Cutulas',
+    breed: 'Beagle',
+    imgSrc: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2021/01/13180313/Beagle-puppy-standing-in-the-grass-1.jpeg'
+  }, {
+    id: '4',
+    name: 'Prince',
+    breed: 'Golden Retriever',
+    imgSrc: 'https://www.ctvsh.com/sites/default/files/styles/large/public/golden-retriever-dog-breed-info.jpg?itok=KV7Ojj-C'
+  },
+];
--- JavaScript - DOM/42 pamoka - komponentų atnaujinimas/teorija/js/main.js
+++ JavaScript - DOM/42 pamoka - komponentų atnaujinimas/teorija/js/main.js
@@ -1,13 +1,23 @@
 const rootElement = document.querySelector('#root');
 
 const formatedUserDataForTableComponent = userDataArr.reduce((result, { imgSrc, email, role }) => {
-  result.push([imgSrc, email, role]);
+  result.push([`<img class="table__img "src="${imgSrc}" />`, email, role]);
   return result;
 }, []);
 
-const tableComponent = new TableComponent({
+const userTableComponent = new TableComponent({
   colNames: ['Nuotrauka', 'El. paštas', 'Rolė'],
   data: formatedUserDataForTableComponent
 });
 
-rootElement.appendChild(tableComponent.htmlElement);
+const dogTableComponent = new TableComponent({
+  colNames: ['Nuotrauka', 'Vardas', 'Veislė', ],
+  data: dogDataArr.reduce((rowData, { name, breed, imgSrc }) => [...rowData, [
+    `<img class="table__img "src="${imgSrc}" />`,
+    name,
+    breed,
+  ]], [])
+});
+
+rootElement.appendChild(userTableComponent.htmlElement);
+rootElement.appendChild(dogTableComponent.htmlElement);
