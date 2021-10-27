98fc3e0b02934f6841f77f97599214793d4c1277
RokBan <rokas.banaitis@gmail.com>
2021-10-27 12:26:23 +0300

    Lesson 42 - tasks answers

--- JavaScript - DOM/42 pamoka - komponentų atnaujinimas/praktika/JS DOM ReadDelete2/data.js
+++ JavaScript - DOM/42 pamoka - komponentų atnaujinimas/praktika/JS DOM ReadDelete2/data.js
@@ -5,6 +5,7 @@ const moviesData = [
 		buttons: ["Delete this element", "Generate movies from the list"],
 		movies: [
 			{
+				id : "MUIM2008",
 				actors: ['Robert Downey Jr.', 'Terrence Howard', 'Jeff Bridges', 'Gwynth Paltrow', 'Leslie Bibb'],
 				IMDB: 7.9,
 				releaseYear: 2008,
@@ -15,6 +16,7 @@ const moviesData = [
 				poster: "https://pics.filmaffinity.com/Ironman-108960873-large.jpg",
 				description: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil."
 			}, {
+				id : "MUT2011",
 				actors: ['Chris Hemsworth', 'Natalie Portman', 'Tom Hiddleston', 'Anthony Hopkins', 'Stellan Skarsgard'],
 				IMDB: 7,
 				releaseYear: 2011,
@@ -26,6 +28,7 @@ const moviesData = [
 				description: "The powerful but arrogant god Thor is cast out of Asgard to live amongst humans in Midgard (Earth), where he soon becomes one of their finest defenders."
 
 			}, {
+				id : "LEG2003",
 				actors: ['Sean Connery', 'Naseeruddin Shah', 'Peta Wilson', 'Tony Curran', 'Stuart Townsend'],
 				IMDB: 5.8,
 				releaseYear: 2003,
@@ -36,6 +39,7 @@ const moviesData = [
 				poster: "https://i.pinimg.com/736x/fe/18/28/fe1828d116d1eb99f8eebe36873f786c.jpg",
 				description: "In an alternate Victorian Age world, a group of famous contemporary fantasy, science fiction, and adventure characters team up on a secret mission."
 			}, {
+				id : "MUSM2002",
 				actors: ['Tobey Maguire', 'Willem Dafoe', 'Kirsten Dunst', 'James Franco', 'Cliff Robertson'],
 				IMDB: 7.3,
 				releaseYear: 2002,
@@ -46,6 +50,7 @@ const moviesData = [
 				poster: "https://m.media-amazon.com/images/I/51kzFX8Zr8L._AC_.jpg",
 				description: "When bitten by a genetically modified spider, a nerdy, shy, and awkward high school student gains spider-like abilities that he eventually must use to fight evil as a superhero after tragedy befalls his family."
 			}, {
+				id : "MUDP2016",
 				actors: ['Ryan Reynolds', 'Karan Soni', 'Ed Skrein', 'Michael Benyaer', 'Brianna Hildebrand'],
 				IMDB: 8,
 				releaseYear: 2016,
@@ -56,6 +61,7 @@ const moviesData = [
 				poster: "https://chickflickingreviews.files.wordpress.com/2016/02/deadpool-valentinesday_.jpg",
 				description: "A wisecracking mercenary gets experimented on and becomes immortal but ugly, and sets out to track down the man who ruined his looks."
 			}, {
+				id : "VH2004",
 				actors: ['Hugh Jackman', 'Kate Beckinsale', 'Richard Roxburgh', 'David Wenham', 'Shuler Hensley'],
 				IMDB: 6.1,
 				releaseYear: 2004,
@@ -66,6 +72,7 @@ const moviesData = [
 				poster: "https://m.media-amazon.com/images/I/71DHEagosIL._AC_SY679_.jpg",
 				description: "The famed monster hunter is sent to Transylvania to stop Count Dracula, who is using Dr. Frankenstein's research and a werewolf for nefarious purposes."
 			}, {
+				id : "MUXM2000",
 				actors: ['Hugh Jackman', 'Patrick Stewart', 'Ian McKellen', 'Famke Janssen', 'James Marsden'],
 				IMDB: 7.4,
 				releaseYear: 2000,
@@ -76,6 +83,7 @@ const moviesData = [
 				poster: "https://m.media-amazon.com/images/M/MV5BZmIyMDk5NGYtYjQ5NS00ZWQxLTg2YzQtZDk1ZmM4ZDBlN2E3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
 				description: "In a world where mutants (evolved super-powered humans) exist and are discriminated against, two groups form for an inevitable clash: the supremacist Brotherhood, and the pacifist X-Men."
 			}, {
+				id : "MUAM2015",
 				actors: ['Paul Rudd', 'Michael Douglas', 'Evangeline Lilly', 'Corey Stoll', 'Bobby Cannavale'],
 				IMDB: 7.3,
 				releaseYear: 2015,
@@ -86,6 +94,7 @@ const moviesData = [
 				poster: "https://m.media-amazon.com/images/I/71E9abm2ayL._AC_SL1111_.jpg",
 				description: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help his mentor, Dr. Hank Pym, pull off a plan that will save the world."
 			}, {
+				id : "ME2018",
 				actors: ['Hera Hilmar', 'Robert Sheehan', 'Hugo Weaving', 'Jihae', 'Ronan Raftery'],
 				IMDB: 6.1,
 				releaseYear: 2018,
@@ -96,6 +105,7 @@ const moviesData = [
 				poster: "https://m.media-amazon.com/images/I/91qiKWG+CrL._AC_SY741_.jpg",
 				description: "In a post-apocalyptic world where cities ride on wheels and consume each other to survive, two people meet in London and try to stop a conspiracy."
 			}, {
+				id : "ZL2009",
 				actors: ['Jesse Eisenberg', 'Woody Harrelson', 'Emma Stone', 'Abigail Breslin'],
 				IMDB: 7.6,
 				releaseYear: 2009,
@@ -106,6 +116,7 @@ const moviesData = [
 				poster: "https://m.media-amazon.com/images/M/MV5BMTU5MDg0NTQ1N15BMl5BanBnXkFtZTcwMjA4Mjg3Mg@@._V1_.jpg",
 				description: "A shy student trying to reach his family in Ohio, a gun-toting bruiser in search of the last Twinkie and a pair of sisters striving to get to an amusement park join forces in a trek across a zombie-filled America."
 			}, {
+				id : "DCUBB2005",
 				actors: ['Christian Bale', 'Michael Caine', 'Liam Neeson', 'Katie Holmes', 'Gary Oldman'],
 				IMDB: 8.2,
 				releaseYear: 2005,
@@ -116,6 +127,7 @@ const moviesData = [
 				poster: "https://cdn.shopify.com/s/files/1/1416/8662/products/batman_begins_2005_advance_original_film_art_21d23979-c416-4ccf-ab5a-bfc32eefda10_1200x.jpg?v=1607193476",
 				description: "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from corruption."
 			}, {
+				id : "MEULotR2001",
 				actors: ['Sean Astin', 'Orlando Bloom', 'Elijah Wood', 'Ian McKellen', 'Viggo Mortensen'],
 				IMDB: 8.8,
 				releaseYear: 2001,
@@ -126,6 +138,7 @@ const moviesData = [
 				poster: "https://m.media-amazon.com/images/I/81EBp0vOZZL._AC_SY741_.jpg",
 				description: "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron."
 			}, {
+				id : "MEUHUJ2012",
 				actors: ['Ian McKellen', 'Martin Freeman', 'Richard Armitage', 'Ken Stott', 'Graham McTavish'],
 				IMDB: 7.8,
 				releaseYear: 2012,
@@ -136,6 +149,7 @@ const moviesData = [
 				poster: "https://m.media-amazon.com/images/I/71E9rZRPOLL._AC_SL1333_.jpg",
 				description: "A reluctant Hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home, and the gold within it from the dragon Smaug."
 			}, {
+				id : "UUU2003",
 				actors: ['Kate Beckinsale', 'Scott Speedman', 'Michael Sheen', 'Shane Brolly', 'Bill Nighy'],
 				IMDB: 7.0,
 				releaseYear: 2003,
