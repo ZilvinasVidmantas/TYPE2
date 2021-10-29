f66c6e39732dd9972330d0784019cc3df4b7796c
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-10-29 08:48:42 +0300

    lesson 45: manager task

---
+++ JavaScript - DOM/45 pamoka - komponentų atnaujinimas 4/teorija/js/main.js
@@ -0,0 +1,52 @@
+const rootElement = document.querySelector('#root');
+
+const userForm = new FormComponent({
+  title: 'Pridėti vartotoją',
+  fields: [
+    { name: 'role', type: 'text', title: 'Rolė' },
+    { name: 'email', type: 'email', title: 'El. Paštas' },
+    { name: 'imgSrc', type: 'text', title: 'Nuotraukos nuoroda' },
+  ],
+  onSubmit: data => console.log('as esu isoreje', data)
+});
+const formatedUserDataForTableComponent = userDataArr.reduce((result, { id, imgSrc, email, role }) => {
+  result.push({
+    id,
+    rowData: [`<img class="table__img "src="${imgSrc}" />`, email, role]
+  });
+  return result;
+}, []);
+
+const userTableComponent = new TableComponent({
+  colNames: ['Nuotrauka', 'El. paštas', 'Rolė'],
+  data: formatedUserDataForTableComponent
+});
+
+
+/*
+  1. Sukurti UserManagerComponent, kuris atvaizduotų letelę ir formą viens šalia kito
+    1.1 Sukurti failą ir įtraukti
+
+    1.2 persirašyti stndartinius komponeneto metodus: 
+      constructor - priskirti props, iškviesti intialize
+      initialize - atlikti pradinius veiksmus
+      render - kol kas nieko (tuščias metodas)
+
+    1.3 Sukurti funkciją createUser, kuri atspausdintų 1 parametru gautus duomenis
+
+    1.4 initialize metode sukurti 2 komponento savybes:
+      this.table = lentelės komponeneto objektas: 
+        su duomenimis, tokiais pat, kaip main.js faile:
+      this.form = formos komponeneto objektas 
+        su duomenimis, tokiais pat, kaip main.js faile, BET ON SUBMIT FUNKCIJA TURI RODYTI [1.3] funkciją
+
+    1.5 render metode, panaudoti this.table ir this.form komponenetus, kad sukurti atvaizdavimą viens šalia kito
+*/
+
+rootElement.append(
+  userForm.htmlElement,
+  userTableComponent.htmlElement,
+);
+
+
+
