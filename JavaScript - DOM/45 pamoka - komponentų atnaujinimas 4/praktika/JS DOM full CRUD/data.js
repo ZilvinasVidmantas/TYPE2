2741595ac7ba42e57db8fab9f9695434a245b055
RokBan <rokas.banaitis@gmail.com>
2021-10-29 16:03:48 +0300

    Lesson 45 - tasks answer

--- JavaScript - DOM/45 pamoka - komponentų atnaujinimas 4/praktika/JS DOM full CRUD/data.js
+++ JavaScript - DOM/45 pamoka - komponentų atnaujinimas 4/praktika/JS DOM full CRUD/data.js
@@ -1,18 +1,20 @@
-const animals = [{
-	title : "Animals",
-	paragraph : "Animals I like...",
-	buttons : ["Create", "Exit", "Edit", "Submit", "Delete"],
-	animals : [
-		{
-			animal : "cat",
-			breed : "Scottish Fold",
-			photo : "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Scottish_fold_cat.jpg/220px-Scottish_fold_cat.jpg",
-			info : "The Scottish Fold is a breed of domestic cat with a natural dominant-gene mutation that affects cartilage throughout the body, causing the ears to 'fold', bending forward and down towards the front of the head, which gives the cat what is often described as an 'owl-like' appearance."
-		},{
-			animal: "Dog",
-			breed : "German Shepherd",
-			photo : "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12213218/German-Shepherd-on-White-00.jpg",
-			info : "Generally considered dogkind's finest all-purpose worker, the German Shepherd Dog is a large, agile, muscular dog of noble character and high intelligence. Loyal, confident, courageous, and steady, the German Shepherd is truly a dog lover's delight."
-		}
-	]
-}];
+const animals = [
+	{
+		title : "Animals",
+		paragraph : "Animals I like Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil iste quae sed cum, laudantium ipsa quis animi quaerat odit maxime accusamus voluptatibus assumenda numquam sequi repellendus dolorem dolorum. Officia, laborum?",
+		buttons : ["Create", "Exit", "Edit", "Submit", "Delete"],
+		animals : [
+			{
+				animal : "Cat",
+				breed : "Scottish Fold",
+				photo : "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Scottish_fold_cat.jpg/220px-Scottish_fold_cat.jpg",
+				info : "The Scottish Fold is a breed of domestic cat with a natural dominant-gene mutation that affects cartilage throughout the body, causing the ears to 'fold', bending forward and down towards the front of the head, which gives the cat what is often described as an 'owl-like' appearance."
+			},{
+				animal: "Dog",
+				breed : "German Shepherd",
+				photo : "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12213218/German-Shepherd-on-White-00.jpg",
+				info : "Generally considered dogkind's finest all-purpose worker, the German Shepherd Dog is a large, agile, muscular dog of noble character and high intelligence. Loyal, confident, courageous, and steady, the German Shepherd is truly a dog lover's delight."
+			}
+		]
+	}
+];
