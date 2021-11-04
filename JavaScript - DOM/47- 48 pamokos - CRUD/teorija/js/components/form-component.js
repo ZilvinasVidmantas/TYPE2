9249c664aa198135e4351eda3ed36232aa0295a0
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-04 08:04:27 +0200

    lesson 47: init

---
+++ JavaScript - DOM/47- 48 pamokos - CRUD/teorija/js/components/form-component.js
@@ -0,0 +1,94 @@
+class FormComponent {
+  /**
+   * Atliekamas komponento sukūrimas, sukuriami props ir state kintamieji
+   * props - iš išorės perduoti kintamieji
+   * state - kintamieji kuriuos keis komponentas
+   * 
+   * @param {Object} props - perduoti kintamieji komponentui
+   */
+  constructor(props) {
+    this.props = clone(props);
+    this.initialize();
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = this.fields.reduce((res, field) => {
+      res[field.name] = field.value;
+      return res;
+    }, {});
+    this.props.onSubmit(formData);
+  }
+
+  createFieldString = ({ name, type, title }) => `
+    <div class="mb-3">
+      <label for="${name}" class="form-label">${title}</label>
+      <input type="${type}" class="form-control" id="${name}" name="${name}">
+    </div>`;
+
+  /**
+   * atliekami pradiniai komponento formavimo veiksmai
+   */
+  initialize = () => {
+    const { title, fields } = this.props;
+    const fieldsString = fields.map(this.createFieldString).join('');
+
+    this.htmlElement = document.createElement('form');
+    this.htmlElement.className = 'p-3 shadow rounded';
+    this.htmlElement.addEventListener('submit', this.handleSubmit);
+    this.htmlElement.innerHTML = `
+    <h2>${title}</h2>
+    ${fieldsString}
+    <div class="text-center">
+      <button class="btn btn-success">Išsaugoti</button>
+    </div>`;
+    
+    this.fields = Array.from(this.htmlElement.querySelectorAll('[name]'));
+  }
+
+  /**
+   * Atliekami veiksmai pasikeitus komponento duomenims
+   */
+  render = () => {
+
+  }
+}
+
+let people = [
+  { name: 'Alice', age: 21 },
+  { name: 'Max', age: 20 },
+  { name: 'Jane', age: 20 }
+];
+
+//                               'age'
+function groupBy(objectArray, property) {
+  return objectArray.reduce((acc, obj) => {
+    //        obj['age']     21
+    //        obj['age']     20
+    //        obj['age']     20
+    let key = obj[property]
+    if (!acc[key]) {
+      acc[key] = []
+    }
+    acc[key].push(obj)
+    return acc; 
+    /* 
+      1: {
+        21: [{ name: 'Alice', age: 21 }]
+      }
+
+      2: { 
+        21: [{ name: 'Alice', age: 21 }],
+        20: [{ name: 'Max', age: 20 }]
+      }
+
+      3: { 
+        21: [{ name: 'Alice', age: 21 }],
+        20: [{ name: 'Max', age: 20 }, { name: 'Jane', age: 20 }]
+      }
+
+    */
+  }, {})
+}
+
+let groupedPeople = groupBy(people, 'age')
---
+++ JavaScript - DOM/47- 48 pamokos - CRUD/teorija/js/components/user-manager-component.js
@@ -0,0 +1,87 @@
+let idBasis = 88;
+
+const generateId = () => String(idBasis++);
+
+// state - komponento būsena, kuri keičiasi programos gyvavimo metu
+// state - component state, which changes at runtime
+// props - pradiniai komponeneto duomenys, kurie nekeičiami komponento viduje (nebent juos keistų išoriniai komponenetai)
+// props - initial component data, which do not change insede component logic (unless they ar updated from outside)
+class UserManagerComponent {
+  constructor() {
+    this.state = {
+      data: clone(userDataArr) // Ateityje, aš šiuos duomenis parisiųsiu iš serverio
+    };
+    this.initialize();
+  }
+
+  // Pritaiko vartotojų duomenis lentelei
+  formatTableData = () => this.state.data.map(({ id, imgSrc, email, role }) => ({
+    id,
+    rowData: [`<img class="table__img "src="${imgSrc}" />`, email, role]
+  }))
+
+  // Sukuria naują vartotoją
+  createUser = (formData) => {
+    const user = {
+      ...formData,
+      id: generateId()
+    };
+    this.state.data.push(user);
+    this.render();
+  }
+
+  // Ištrina vartotoją
+  deleteUser = (id) => {
+    this.state.data = this.state.data.filter(user => user.id !== id);
+    this.render();
+  }
+
+  // TODO: įgalina vartotjo redagavimą
+  editUser = (id) =>{
+    console.log('atnaujinamas vartotojas:', id);
+  }
+
+  initializeForm = () => {
+    this.form = new FormComponent({
+      title: 'Pridėti vartotoją',
+      fields: [
+        { name: 'role', type: 'text', title: 'Rolė' },
+        { name: 'email', type: 'email', title: 'El. Paštas' },
+        { name: 'imgSrc', type: 'text', title: 'Nuotraukos nuoroda' },
+      ],
+      onSubmit: this.createUser
+    });
+
+    const formContainer = document.createElement('div');
+    formContainer.className = 'col-12 col-lg-3';
+    formContainer.appendChild(this.form.htmlElement);
+    this.htmlElement.appendChild(formContainer);
+  }
+  initializeTable = () => {
+    this.table = new TableComponent({
+      colNames: ['Nuotrauka', 'El. paštas', 'Rolė'],
+      data: this.formatTableData(),
+      onDelete:  this.deleteUser,
+      onEdit: this.editUser
+    });
+
+    const tableContainer = document.createElement('div');
+    tableContainer.className = 'col-12 col-lg-9';
+    tableContainer.appendChild(this.table.htmlElement);
+    this.htmlElement.appendChild(tableContainer);
+  }
+
+  initialize = () => {
+    this.htmlElement = document.createElement('div');
+    this.htmlElement.className = 'row flex-lg-row-reverse g-3';
+
+    this.initializeForm();
+    this.initializeTable();
+  }
+
+  render = () => {
+    this.table.updateProps({
+      data: this.formatTableData()
+    });
+  }
+}
