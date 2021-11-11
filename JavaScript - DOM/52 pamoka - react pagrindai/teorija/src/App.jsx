843dd5b120216a320e294a4173fb71217a9a497d
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-11 12:03:50 +0200

    lesson 52: component module styles

--- JavaScript - DOM/52 pamoka - react pagrindai/teorija/src/App.jsx
+++ JavaScript - DOM/52 pamoka - react pagrindai/teorija/src/App.jsx
@@ -1,35 +1,26 @@
 import React from 'react';
-import Paragraph from './components/Paragraph';
-import Button from './components/Button';
-import Input from './components/Input';
+import InputField from './components/InputField';
 class App extends React.Component {
 
   render() {
     return (
       <div>
         <h1>Čia yra appsas</h1>
-        <Paragraph text="tekstas 1"/>
-        <Paragraph text="tekstas 2"/>
-        <Button content="click 1"/>
-        <Button content="click 2"/>
-        <Input type="text" value="Jonas"/>
-        <Input type="number" value="55"/>
+        <InputField
+          name="Email"
+          value="virgis@gmail.com"
+          type="email"
+          id="input-email"
+        />
+        <InputField
+          name="Password"
+          value=""
+          type="password"
+          id="input-password"
+        />
       </div>
     );
   }
 }
 
 export default App;
-
-/*
-  Sukurkite komponenetą InputField
-  Kuriame būtų:
-    div - gaubiantis elementas
-    label - susietas su įvesties lauku
-    input - įvesties laukas
-  Panaudokite tokius props:
-    name
-    value
-    type
-    id
-*/
