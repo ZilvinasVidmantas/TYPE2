9d847d1df02820ae13cf75cb8158be4bb56c3561
RokBan <rokas.banaitis@gmail.com>
2021-11-15 14:59:35 +0200

    Lesson 54 - updated tasks

--- React/54 pamoka - forms/praktika/src/components/Form.js
+++ React/54 pamoka - forms/praktika/src/components/Form.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import Data from "../data";
 
 const Form = () => {
   const [title, setTitle] = useState("");
@@ -10,45 +11,58 @@ const Form = () => {
     e.preventDefault();
     //patikrinti ar įvestys atitinka reikalavimus
     const post = { title, body, author, id: uuidv4() };
-    //papildyti duomenų failą sukurtu post'u
-    console.log(post);
+    //Data.push(post);
+    //^papildyti duomenų failą sukurtu post'u
+    console.log(Data);
+  }
+
+  const updatePosts = () => {
+    document.querySelector(".updatedPosts").innerHTML = "cia data.map'as"; // Data.map() DOM
   }
 
   return (
-    <div className="create">
-      <h1 className="formName">Add new Post</h1>
-      <form onSubmit={newPost}>
-        <label>Post title:</label>
-        <input
-          type="text"
-          required
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+    <section>
+      <div className="create">
+        <h1 className="formName">Add new Post</h1>
+        <form onSubmit={newPost}>
+          <label>Post title:</label>
+          <input
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
 
-        <label>Post body:</label>
-        <textarea
-          required
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-        ></textarea>
+          <label>Post body:</label>
+          <textarea
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
 
-        <label>Post author:</label>
-        <select
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        >
-          <option value="Jhonny">Jhonny</option>
-          <option value="Bravo">Bravo</option>
-        </select>
-        <button>Add Post</button>
-      </form>
-      {/*
-      <p> {title} </p>
-      <p> {body} </p>
-      <p> {author} </p>
-      */}
-    </div>
+          <label>Post author:</label>
+          <select
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          >
+            <option value="Jhonny">Jhonny</option>
+            <option value="Bravo">Bravo</option>
+          </select>
+          <button>Add Post</button>
+        </form>
+        {/*
+        <p> {title} </p>
+        <p> {body} </p>
+        <p> {author} </p>
+        */}
+      </div>
+      <div className="Posts">
+        <button onClick={updatePosts}>Update</button>
+        <div className="updatedPosts">
+
+        </div>
+      </div>
+    </section>
   );
 }
 
---
+++ React/54 pamoka - forms/praktika/src/data.js
@@ -0,0 +1,10 @@
+const posts = [
+  {
+    title: "kazkoks title",
+    body: "kazkoks body",
+    author: "author baby",
+    id : "??????????"
+  }
+];
+
+export default posts;
