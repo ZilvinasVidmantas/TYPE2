8c2ccddfafde4891f47c17b5f1aca6be30d2c9c0
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-16 10:26:32 +0200

    lesson 55: dynamic field generation

--- React/55 pamoka - formos/teorija/src/components/Form.jsx
+++ React/55 pamoka - formos/teorija/src/components/Form.jsx
@@ -4,24 +4,38 @@ import validator from 'validator';
 import styles from './Form.module.css';
 
 class Form extends React.Component {
-  state = {
-    email: {
-      value: '',
-      error: null,
-      validate: (val) => validator.isEmail(val) ? null : 'Netinkamas pašto formatas'
-    },
-    password: {
-      value: '',
-      error: null,
-      validate: (val) => validator.isStrongPassword(val, { minSymbols: 0 })
-        ? null
-        : 'Slaptažodis turi būti mažiausiai 8 simbolių. Jame turi būti nors 1 dižioji, nors 1 mažoji raidės ir nors vienas skaičius'
-    }
-  };
+  static #formCount = 0;
+
+  constructor(props) {
+    super(props);
+
+    Form.#formCount++;
+
+    this.state = {
+      formNum: Form.#formCount,
+      fields: {
+        email: {
+          value: '',
+          error: null,
+          validate: (val) => validator.isEmail(val) ? null : 'Netinkamas pašto formatas'
+        },
+        password: {
+          value: '',
+          error: null,
+          validate: (val) => validator.isStrongPassword(val, { minSymbols: 0 })
+            ? null
+            : 'Slaptažodis turi būti mažiausiai 8 simbolių. Jame turi būti nors 1 dižioji, nors 1 mažoji raidės ir nors vienas skaičius'
+        }
+      }
+    };
+
+  }
 
   isValid = () => {
-    for (const field in this.state) {
-      if (this.state[field].error !== null) {
+    const { fields } = this.state;
+
+    for (const name in fields) {
+      if (fields[name].error !== null) {
         return false;
       }
     }
@@ -29,11 +43,21 @@ class Form extends React.Component {
   }
 
   handleFieldChange = (name, value) => {
+    const { fields } = this.state;
+    console.log({
+      fields,
+      name,
+      value
+    })
+
     this.setState({
-      [name]: {
-        ...this.state[name],
-        error: this.state[name].validate(value),
-        value
+      fields: {
+        ...fields,
+        [name]: {
+          ...fields[name],
+          error: fields[name].validate(value),
+          value
+        }
       }
     });
   }
@@ -45,9 +69,24 @@ class Form extends React.Component {
     }
   }
 
+  createFields = () => {
+    const { fields, formNum } = this.state;
+
+    return Object.entries(fields).map(([name, { value, error }]) => (
+      <InputField
+        key={name}
+        name={name}
+        value={value}
+        type="text"
+        id={`form${formNum}_${name}`}
+        handleChange={(value) => this.handleFieldChange(name, value)}
+        error={error}
+      />
+    ));
+  }
+
   render() {
     const { title, submitBtnText } = this.props;
-    const { email, password } = this.state;
     const buttonClassName = this.isValid() ? styles.btn : `${styles.btn}  ${styles.btnMuted}`;
 
     const defaultSubmitBtnText = submitBtnText ?? 'Submit';
@@ -55,26 +94,11 @@ class Form extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <h2>{title}</h2>
-        <InputField
-          name="email"
-          value={email.value}
-          type="text"
-          id="input-email"
-          handleChange={(value) => this.handleFieldChange("email", value)}
-          error={email.error}
-        />
-        <InputField
-          name="password"
-          value={password.value}
-          type="password"
-          id="input-password"
-          handleChange={(value) => this.handleFieldChange("password", value)}
-          error={password.error}
-        />
+        {this.createFields()}
         <button type="submit" className={buttonClassName}>{defaultSubmitBtnText}</button>
       </form>
     );
   }
 }
-// 9:20
+
 export default Form;
