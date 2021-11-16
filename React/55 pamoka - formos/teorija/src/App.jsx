f866026d2532995c23c0d1a0d1e498d957305b46
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-16 08:05:25 +0200

    lesson 55: initial

---
+++ React/55 pamoka - formos/teorija/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Form from './components/Form';
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <main>
+        <h1>Aplikacijos pavadinimas</h1>
+        <Form title="Registracija" submitBtnText="Registruotis"/>
+        <Form title="Prisijungimas"/>
+      </main>
+    );
+  }
+}
+
+export default App;
+
+/*
+  Įgalinkite formos komponenetui, dinamišką mygtuko tekstą, naudojant this.props.submitBtnText
+  11:55
+*/
---
+++ React/55 pamoka - formos/teorija/src/components/Form.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import InputField from './InputField';
+import validator from 'validator';
+import styles from './Form.module.css';
+
+class Form extends React.Component {
+  state = {
+    email: {
+      value: '',
+      error: null,
+      validate: (val) => validator.isEmail(val) ? null : 'Netinkamas pašto formatas'
+    },
+    password: {
+      value: '',
+      error: null,
+      validate: (val) => validator.isStrongPassword(val, { minSymbols: 0 })
+        ? null
+        : 'Slaptažodis turi būti mažiausiai 8 simbolių. Jame turi būti nors 1 dižioji, nors 1 mažoji raidės ir nors vienas skaičius'
+    }
+  };
+
+  isValid = () => {
+    for (const field in this.state) {
+      if (this.state[field].error !== null) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  changeEmail = (value) => {
+    this.setState({
+      email: {
+        ...this.state.email,
+        error: this.state.email.validate(value),
+        value
+      }
+    });
+  }
+
+  changePassword = (value) => {
+    this.setState({
+      password: {
+        ...this.state.password,
+        error: this.state.password.validate(value),
+        value
+      }
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.isValid()) {
+      // tai kas daroma, jeigu forma sėkimnga
+    }
+  }
+
+  render() {
+    const { title, submitBtnText } = this.props;
+    const { email, password } = this.state;
+    const buttonClassName = this.isValid() ? styles.btn : `${styles.btn}  ${styles.btnMuted}`;
+
+    const defaultSubmitBtnText = submitBtnText ?? 'Submit';
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>{title}</h2>
+        <InputField
+          name="email"
+          value={email.value}
+          type="text"
+          id="input-email"
+          handleChange={this.changeEmail}
+          error={email.error}
+        />
+        <InputField
+          name="password"
+          value={password.value}
+          type="password"
+          id="input-password"
+          handleChange={this.changePassword}
+          error={password.error}
+        />
+        <button type="submit" className={buttonClassName}>{defaultSubmitBtnText}</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
