9e81a9cfb3905f37560b754aa2c1f8a6c9bcc1db
RokBan <rokas.banaitis@gmail.com>
2021-11-17 13:28:34 +0200

    Lesson 56 - task

---
+++ React/56 pamoka - form submit and tow-way-binded-select/praktika/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from './Form.module.css';
+
+class Form extends React.Component {
+	render() {
+		return (
+			<div className="form">
+				<form>
+					
+					<button> Nuomotis mašiną </button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default Form;
