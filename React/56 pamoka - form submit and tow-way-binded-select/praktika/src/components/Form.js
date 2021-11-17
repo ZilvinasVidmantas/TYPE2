c8cabc67176423dc2120b4c4f3e5c8b4a838bc8d
RokBan <rokas.banaitis@gmail.com>
2021-11-17 15:08:18 +0200

    Lesson 56 - inputs and data

--- React/56 pamoka - form submit and tow-way-binded-select/praktika/src/components/Form.js
+++ React/56 pamoka - form submit and tow-way-binded-select/praktika/src/components/Form.js
@@ -6,7 +6,33 @@ class Form extends React.Component {
 	rentACar = (e) => {
 		e.preventDefault();
 
-		console.log(e.target);
+		console.dir(e.target.querySelector("#car").selectedOptions[0].innerHTML);
+
+		const rentData = this.constructData(e.target);
+		const isValid = this.isDataValid(rentData);
+
+		console.log(rentData);
+	}
+
+	isDataValid = (data) => {
+		let dataIsValid = true;
+
+	}
+
+	constructData = (info) => {
+		const data = {
+			fullname : info.querySelector("#fullName").value,
+			email : info.querySelector("#email").value,
+			phone : info.querySelector("#phone").value,
+			birthDate : info.querySelector("#birth").value,
+			pickLocation : info.querySelector("#pickLocation").selectedOptions[0].innerHTML,
+			pickDate : info.querySelector("#pickDate").value,
+			dropLocation : info.querySelector("#dropLocation").selectedOptions[0].innerHTML,
+			dropDate : info.querySelector("#dropDate").value,
+			car : info.querySelector("#car").selectedOptions[0].innerHTML,
+			extraRequests : info.querySelector("#extraInfo").innerHTML
+		}
+		return data;
 	}
 
 	render() {
@@ -16,40 +42,60 @@ class Form extends React.Component {
 					<h3> Renter Information </h3>
 
 					<label htmlFor="fullName">Name and Surname</label>
-					<input type="text" name="fullName" id="fullName" />
+					<input required type="text" name="fullName" id="fullName" />
 
 					<label htmlFor="email">Email</label>
-					<input type="text" name="email" id="email" />
+					<input required type="email" name="email" id="email" />
 
 					<label htmlFor="phone">Phone</label>
-					<input type="text" name="phone" id="phone" />
+					<input required type="tel" name="phone" id="phone" 
+						pattern="86-[0-9]{3}-[0-9]{4}" placeholder="86-666-6666"
+					/>
 
 					<label htmlFor="birth">Birth date</label>
-					<input type="text" name="birth" id="birth" />
+					<input required type="date" name="birth" id="birth"/>
 
 
 					<h3> Rent Information </h3>
 					
 					<label htmlFor="pickLocation">Pick-up Location</label>
-					<input type="text" name="pickLocation" id="pickLocation" />
+					<select required name="pickLocation" id="pickLocation">
+						<option value="">---</option>
+						<option value="1">Kaunas</option>
+						<option value="2">Vilnius</option>
+						<option value="3">Klaipėda</option>
+						<option value="4">Siesartis</option>
+					</select>
 					
 					<label htmlFor="pickDate">Pick-up Date</label>
-					<input type="text" name="pickDate" id="pickDate" />
+					<input required type="datetime-local" name="pickDate" id="pickDate" />
 
 					<label htmlFor="dropLocation">Drop-off Location</label>
-					<input type="text" name="dropLocation" id="dropLocation" />
+					<select required name="dropLocation" id="dropLocation">
+						<option value="">---</option>
+						<option value="1">Kaunas</option>
+						<option value="2">Vilnius</option>
+						<option value="3">Klaipėda</option>
+						<option value="4">Siesartis</option>
+					</select>
 
 					<label htmlFor="dropDate">Drop-off Date</label>
-					<input type="text" name="dropDate" id="dropDate" />
+					<input required type="datetime-local" name="dropDate" id="dropDate" />
 
 
 					<h3> Rental Car Information </h3>
 
 					<label htmlFor="car">Car</label>
-					<input type="text" name="car" id="car" />
+					<select required name="car" id="car">
+						<option value="">---</option>
+						<option value="1">Audi TT</option>
+						<option value="2">VW Golf</option>
+						<option value="3">BMW 6</option>
+						<option value="4">Fiat Punto</option>
+					</select>
 
 					<label htmlFor="extraInfo">Additional Requests</label>
-					<input type="text" name="extraInfo" id="extraInfo" />
+					<textarea name="extraInfo" id="extraInfo" cols="50" rows="6"></textarea>
 
 
 					<button className={styles.formBtn}> Nuomotis mašiną </button>
