59cfa2b41a6df840b811347132a27ed505c88122
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-17 11:03:14 +0200

    lesson 56: select field

--- React/56 pamoka - forms and router/teorija/src/App.jsx
+++ React/56 pamoka - forms and router/teorija/src/App.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import Form from './components/Form';
 import validator from 'validator';
+import Form from './components/Form';
+import SelectField from './components/SelectField';
 
 class App extends React.Component {
+  state = {
+    error: null,
+    selectedOption: '',
+    options: [
+      { value: '', text: '-- Pasirinkite' },
+      { value: '1', text: 'Vilnius' },
+      { value: '2', text: 'Kaunas' },
+      { value: '3', text: 'Klaipėda' },
+      { value: '4', text: 'Šeulę' },
+    ],
+    validate: (val) => !validator.isEmpty(val) ? null : 'Privaloma pasirinkti miestą'
+  }
+
+  handleSelectChange = (newOption) => {
+    this.setState({
+      selectedOption: newOption,
+      error: this.state.validate(newOption)
+    })
+  }
+
   doStuff = (data) => {
     console.log(data);
   }
 
   render() {
+    console.log('pasirinkta:', this.state.selectedOption);
+    console.table(this.state.options);
 
     return (
       <main>
@@ -33,6 +56,15 @@ class App extends React.Component {
           ]}
           onSubmit={this.doStuff}
         />
+        <SelectField 
+          name="select-name"
+          value={this.state.selectedOption}
+          label="Miestas"
+          id="select-id"
+          options={this.state.options}
+          error={this.state.error}
+          handleChange={this.handleSelectChange}
+        />
       </main>
     );
   }
@@ -41,6 +73,8 @@ class App extends React.Component {
 export default App;
 
 /*
-  Implementuokite label. Vietoj komentarų įrašykite savo kodą
+  10 min pertrauka
+  analizė
+  11:30 - klausimai
 
 */
