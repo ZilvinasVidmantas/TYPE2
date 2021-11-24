dd6f3ec4b0352193dbc7c6e40ed16b4e46474664
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-11-24 12:10:13 +0200

    lesson: filter logic implemented nad homework

--- React/61 pamoka - lentelė su filtrais 2/teorija/src/App.jsx
+++ React/61 pamoka - lentelė su filtrais 2/teorija/src/App.jsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Typography,
   Grid,
-  Button,
 } from '@mui/material';
 import DataTable from './components/DataTable';
 import DataFilters from './components/DataFilters';
 
 const filters = [
-  { title: 'Markė', property: 'brand' },
-  { title: 'Modelis', property: 'model' },
+  { title: 'Markė', property: 'brand', type: '' },
+  { title: 'Modelis', property: 'model', type: '' },
 ];
 
 const initialCars = [
@@ -22,55 +21,61 @@ const initialCars = [
 ];
 
 const App = () => {
-  const [cars, setCars] = useState((() => {
-    console.log('nustama nauja cars reikšmė')
-    return initialCars
-  })());
+  const [cars, setCars] = useState(initialCars);
 
   const handleFilterGroupChange = (event) => {
-    console.table(filterPropsArray.reduce((options, filterProps) => [...options, ...filterProps.options], []));
-    
     const { value, name, checked } = event.target;
-    const newFilters = filterPropsArray.map(filterProps => ({
+
+    setDataFilterPropsArray((prevState) => prevState.map(filterProps => ({
       ...filterProps,
       options: filterProps.name !== name
-      ? filterProps.options
-      : filterProps.options.map(option => ({
+        ? filterProps.options
+        : filterProps.options.map(option => ({
           ...option,
           checked: option.value === value ? checked : option.checked
         }))
-      }));
-    console.table(newFilters.reduce((options, filterProps) => [...options, ...filterProps.options], []));
-      
-    setDataFilterPropsArray(newFilters);
+    })));
   }
-  // 11:10
-  // 5 min. Kur naudojamas state kintamis aprašant pradinę <filterPropsArray> reikšmę ?
-  const [filterPropsArray, setDataFilterPropsArray] = useState(filters.map(({ title, property }) => {
-    console.log('formuojami FormGroup options');
-    const properties = cars.map(car => car[property]);
-    const uniqProperties = [...new Set(properties)];
-    const options = uniqProperties.map(uniqProp => ({
-      label: uniqProp,
-      value: uniqProp,
-      checked: true
-    }));
 
-    return {
-      title,
-      name: property,
-      options,
-      onChange: handleFilterGroupChange
-    }
-  }));
+  const [filterPropsArray, setDataFilterPropsArray] = useState(
+    filters.map(({ title, property }) => {
+      const properties = cars.map(car => car[property]);
+      const uniqProperties = [...new Set(properties)];
+      const options = uniqProperties.map(uniqProp => ({
+        label: uniqProp,
+        value: uniqProp,
+        checked: true
+      }));
+
+      return {
+        title,
+        name: property,
+        options,
+        onChange: handleFilterGroupChange
+      }
+    })
+  );
+
+  useEffect(() => {
+    // console.log('Reaguojama į filtrų pasikeitimą');
+    console.table(filterPropsArray.reduce((all, props) => [...all, ...props.options], []))
+    setCars(initialCars.filter(car => {
+        for (let i = 0; i < filterPropsArray.length; i++) {
+          const filter = filterPropsArray[i];
+          const validProps = filter.options.filter(x => x.checked).map(x => x.value)
+          if (!validProps.includes(car[filter.name])) {
+            return false;
+          }
+        }
+        return true;
+      })
+    );
+
+  }, [filterPropsArray])
 
   return (
     <Container>
       <Typography component="h1" variant="h3" gutterBottom align="center">Mašinos</Typography>
-      <Button onClick={() => setCars([]) }>change state</Button>
-      <Typography component="pre">
-        {JSON.stringify(cars, undefined, 2)}
-      </Typography>
       <Grid container spacing={2}>
         <Grid item xs={3}>
           <DataFilters filterPropsArray={filterPropsArray} />
