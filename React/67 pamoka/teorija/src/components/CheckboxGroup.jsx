5e7d71aab04cba11f1a4006d841fab4cd0cdbf15
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-12-02 11:52:25 +0200

    lesson 67: homework task

--- React/67 pamoka/teorija/src/components/CheckboxGroup.jsx
+++ React/67 pamoka/teorija/src/components/CheckboxGroup.jsx
@@ -4,11 +4,11 @@ import {
   Checkbox
 } from '@mui/material';
 
-const CheckboxGroup = ({ label, checked, name, value, onChange}) => {
+const CheckboxGroup = ({ label, checked, name, value, onChange }) => {
   return (
     <FormGroup>
       <FormControlLabel
-        control={<Checkbox checked={checked} onChange={onChange} name={name} value={value}/>}
+        control={<Checkbox checked={checked} onChange={onChange} name={name} value={value} />}
         label={label}
       />
     </FormGroup>
--- React/67 pamoka/teorija/src/components/RangeFilter.jsx
+++ React/67 pamoka/teorija/src/components/RangeFilter.jsx
@@ -5,6 +5,17 @@ import { CarContext } from '../contexts/CarContext';
 
 const RangeFilter = ({ filterName, title, selectedMin, selectedMax, min, max }) => {
   const { changeFilters } = useContext(CarContext);
+  /*
+    Yra problema:
+    Konteksto <CarContext> funkcija <changeFilters> kviečiasi labai daug kartų, kuomet judiname Slider'į
+    Reikėtų padaryti, jog funkcija kviestūsi tik atleidus slider'io 'burbuliuką'
+    Tam yra speciali MUI-Slider funkcija <onChangeCommitted>, tačiau, ši funkcija panaikina slankiojimo animaciją.
+
+    Padaryti, jog <CarContext.changeFilters> funkcija kviestūsi tiktais atleidus, bet matytūsi slider'io judinimo animacija
+
+    Vienas iš sprendimų galėtų būti:
+      Sukurti šiame komponente <state> kintamajį animacijai palaikyti, o kviesti <CarContext.changeFilters> tiktais atleidus.
+  */
 
   return (
     <FilterContainer title={title}>
