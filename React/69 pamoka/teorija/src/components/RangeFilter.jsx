6fc63243d5ef9945d803b6493a02561be5133e97
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-12-06 08:03:34 +0200

    lesson 69: init

---
+++ React/69 pamoka/teorija/src/components/RangeFilter.jsx
@@ -0,0 +1,59 @@
+import { useContext, useState, useEffect } from "react";
+import { Slider, Box, Input } from '@mui/material';
+import FilterContainer from './FilterContainer';
+import { CarContext } from '../contexts/CarContext';
+
+const RangeFilter = ({ filterName, title, selectedMin, selectedMax, min, max }) => {
+  const { changeFilters } = useContext(CarContext);
+  const [range, setRange] = useState([selectedMin, selectedMax])
+  const [currMin, currMax] = range;
+
+  const handleSliderOnChange = (_, newRange) => {
+    const [min, max] = newRange;
+    if(min !== currMin || max !== currMax){
+      setRange(newRange);
+    }
+  };
+
+  useEffect(() => setRange([selectedMin, selectedMax]), [selectedMin, selectedMax]);
+
+  return (
+    <FilterContainer title={title}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, my: 1, px: 3 }}>
+        <Input
+          value={currMin}
+          type="number"
+          inputProps={{ sx: { textAlign: 'center' } }}
+          onChange={(event) => changeFilters({
+            filterName,
+            min: Number(event.target.value),
+            max: currMax
+          })}
+        />
+        <Input
+          value={currMax}
+          type="number"
+          inputProps={{ sx: { textAlign: 'center' } }}
+          onChange={(event) => changeFilters({
+            filterName,
+            min: currMin,
+            max: Number(event.target.value)
+          })}
+        />
+      </Box>
+      <Box sx={{ px: 1 }}>
+        {/* TODO: kodėl Keičiant Slider reikšmę kontekstas kviečiasi 2 kartus */}
+        <Slider
+          getAriaLabel={() => 'Temperature range'}
+          min={min}
+          max={max}
+          value={range}
+          onChange={handleSliderOnChange}
+          onChangeCommitted={(_, [min, max]) => changeFilters({ filterName, min, max })}
+          valueLabelDisplay="auto"
+        />
+      </Box>
+    </FilterContainer>);
+};
+
+export default RangeFilter;
