7c0e4e026d760e307af722732dddc8d39b26af8f
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2021-12-31 11:12:52 +0200

    feat(87): Login and Register pages routing

--- React/87 pamoka - redux combine reducers/src/App.jsx
+++ React/87 pamoka - redux combine reducers/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
+import store from './store';
+import Navbar from './components/partials/navbar';
 import TodoAppPage from './pages/todo-app-page';
 import Home from './pages/home-page';
-import Navbar from './components/partials/navbar';
-import store from './store';
+import LoginPage from './pages/login-page';
+import RegisterPage from './pages/register-page';
 
 const App = () => (
   <ReduxProvider store={store}>
@@ -15,6 +17,8 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todo-app" element={<TodoAppPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   </ReduxProvider>
