a7a612356d643d88d866a997e210d697a22925f9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-03 10:09:55 +0200

    feat(88): login logic implemented

--- React/88 pamoka - redux auth/server/src/controllers/auth-controller.js
+++ React/88 pamoka - redux auth/server/src/controllers/auth-controller.js
@@ -3,8 +3,8 @@ const { users } = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
 
 const findUserByEmailAndPassword = (email, password) => {
   const user = users.find(x => x.email === email);
-  if(user){
-    if(user.password === password){
+  if (user) {
+    if (user.password === password) {
       return user;
     }
   }
@@ -14,23 +14,18 @@ const findUserByEmailAndPassword = (email, password) => {
 export const login = (req, res) => {
   const { email, password } = req.body;
   const user = findUserByEmailAndPassword(email, password);
-  /*
-    Patikrinkti kintamajį user, jei user'is rastas, siųsti user'io duomenis, priešingu atveju, siųsti nesėkmingo prisijungimo duomenis
-  */
-  if (true) {
-    // sėkimgas prisijungimas
+  if (user) {
     res.status(200).json({
       status: 200,
       message: 'Pavyko prisijungti',
       token: 'iadhgoisghiohsdghfgh54+sf6gh6dhn54dgh',
-      email: 'admin@gmail.com',
-      role: 'admin'
+      email: user.email,
+      role: user.role
     });
   } else {
-    // nesėkmingas prisijungimas
     res.status(400).json({
       status: 400,
-      message: "Neteisingi prisijungimo duomenys"
+      message: "Nerastas vartotojas"
     });
   }
 };
