90c19cca1478c319cf35e80fd4b8d2b892bde052
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-03 11:22:22 +0200

    task(88): create api-service

---
+++ React/88 pamoka - redux auth/client/src/services/api-service.js
@@ -0,0 +1,30 @@
+/*
+Naudojant 'axios' biblioteką, sukurti asinchronines funkcijas:
+  * įsirašyti axios biblioteką, ir sukurti 'instance'ą užklausoms daryti
+
+  1.
+    pavadinimas: login
+    užklausos adresas:'http://localhost:5000/auth/login':
+    tipas: POST
+    duomenys(body): { email, password } - kolkas į'hard-code'inkite
+
+  2.
+    pavadinimas: register
+    užklausos adresas:'http://localhost:5000/auth/register':
+    tipas: POST
+    duomenys(body): {
+      name,
+      surname,
+      email,
+      password,
+      repeatPassword,
+      subscribed
+    } - kolkas į'hard-code'inkite
+
+  * Šias funkcijas exportuokite atskirai, ir  bendrame objekte su 'export default'
+
+  * Ištestuokite užklausų sekmingus ir nesėkmingus atvejus
+
+  * funkcijoms sukurkite parametrus, ir ištestuokite duomenis perduodant per parametrus
+
+*/
