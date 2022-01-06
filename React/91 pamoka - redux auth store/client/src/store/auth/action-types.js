3bef5f714e18b7ab22f08137f5b122309d144326
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-06 10:32:39 +0200

    feat(91): created redux LOGIN_SUCCESS action

--- React/91 pamoka - redux auth store/client/src/store/auth/action-types.js
+++ React/91 pamoka - redux auth store/client/src/store/auth/action-types.js
@@ -1,5 +1,5 @@
-export const LOGIN = 'LOGIN';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 
 export default {
-  LOGIN,
+  LOGIN_SUCCESS,
 };
--- React/91 pamoka - redux auth store/client/src/store/auth/index.js
+++ React/91 pamoka - redux auth store/client/src/store/auth/index.js
@@ -1,16 +1,22 @@
-import { LOGIN } from './action-types';
+/* eslint-disable no-param-reassign */
+import produce from 'immer';
+import { LOGIN_SUCCESS } from './action-types';
 
 const initState = {
   loggedIn: false,
+  token: null,
   user: null,
 };
 
 // eslint-disable-next-line default-param-last
 const reducer = (state = initState, action) => {
   switch (action.type) {
-    case LOGIN:
-
-      return state;
+    case LOGIN_SUCCESS:
+      return produce(state, (newState) => {
+        newState.loggedIn = true;
+        newState.user = action.payload.user;
+        newState.token = action.payload.token;
+      });
 
     default:
       return state;
