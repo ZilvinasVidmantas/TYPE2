fd13b4bbd3d600b49d11164940ffa5218aebef1e
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-06 11:11:20 +0200

    feat(91): auth data saved to session

--- React/91 pamoka - redux auth store/client/src/store/auth/index.js
+++ React/91 pamoka - redux auth store/client/src/store/auth/index.js
@@ -1,25 +1,28 @@
 /* eslint-disable no-param-reassign */
 import produce from 'immer';
 import { LOGIN_SUCCESS } from './action-types';
+import SessionService from '../../services/session-service';
 
-const initState = {
+const initState = SessionService.get('auth') ?? {
   loggedIn: false,
   token: null,
   user: null,
 };
 
 // eslint-disable-next-line default-param-last
-const reducer = (state = initState, action) => {
+const reducer = (oldState = initState, action) => {
   switch (action.type) {
-    case LOGIN_SUCCESS:
-      return produce(state, (newState) => {
-        newState.loggedIn = true;
-        newState.user = action.payload.user;
-        newState.token = action.payload.token;
+    case LOGIN_SUCCESS: {
+      const newState = produce(oldState, (state) => {
+        state.loggedIn = true;
+        state.user = action.payload.user;
+        state.token = action.payload.token;
       });
-
+      SessionService.set('auth', newState);
+      return newState;
+    }
     default:
-      return state;
+      return oldState;
   }
 };
 
