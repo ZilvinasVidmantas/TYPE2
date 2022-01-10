b77d283d6a665cf408b00aa9d9795b32dc315360
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-10 11:11:01 +0200

    refactor(93): exported navbar-auth-menu

--- React/93 pamoka - autorizuotas puslapis/client/src/components/partials/navbar/index.jsx
+++ React/93 pamoka - autorizuotas puslapis/client/src/components/partials/navbar/index.jsx
@@ -1,21 +1,12 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import {
   AppBar,
-  Divider,
   Container,
-  Menu,
-  MenuItem,
-  IconButton,
-  Avatar,
   Box,
-  Typography,
   styled,
 } from '@mui/material';
-import { useSelector, useDispatch } from 'react-redux';
-import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
-import PersonIcon from '@mui/icons-material/Person';
+import { useSelector } from 'react-redux';
 import { authSelector } from '../../../store/auth/selectors';
-import { logoutAction } from '../../../store/auth/action-creators';
 import ButtonLink from './navbar-link-button';
 import RoutingService from '../../../routing';
 import {
@@ -23,9 +14,8 @@ import {
   TodoAppRoute,
   LoginRoute,
   RegisterRoute,
-  ProfileRoute,
 } from '../../../routing/routes';
-import AuthMenuLink from './navbar-auth-menu-link';
+import AuthMenu from './navbar-auth-menu';
 
 const leftRoutes = [
   HomeRoute,
@@ -43,21 +33,11 @@ const StyledContainer = styled(Container)(({ theme }) => ({
 
 const Navbar = () => {
   // eslint-disable-next-line no-unused-vars
-  const anchorRef = useRef();
   const auth = useSelector(authSelector);
-  const [menuOpen, setMenuOpen] = useState(false);
-  const dispatch = useDispatch();
 
-  const handleOpenMenu = () => setMenuOpen(true);
-  const handleCloseMenu = () => setMenuOpen(false);
-  const handleLogout = () => {
-    handleCloseMenu();
-    dispatch(logoutAction);
-  };
-
-  const authLeftRoutes = leftRoutes
+  const authenticatedLeftRoutes = leftRoutes
     .filter((route) => RoutingService.authenticateRoute(route, auth));
-  const authRightRoutes = rightRoutes
+  const authenticatedRightRoutes = rightRoutes
     .filter((route) => RoutingService.authenticateRoute(route, auth));
 
   return (
@@ -71,49 +51,12 @@ const Navbar = () => {
         }}
         >
           <Box sx={{ display: 'flex' }}>
-            {authLeftRoutes.map((props) => <ButtonLink key={props.link} {...props} />)}
+            {authenticatedLeftRoutes.map((props) => <ButtonLink key={props.link} {...props} />)}
           </Box>
           <Box sx={{ display: 'flex' }}>
-            {authRightRoutes.map((props) => <ButtonLink key={props.link} {...props} />)}
+            {authenticatedRightRoutes.map((props) => <ButtonLink key={props.link} {...props} />)}
           </Box>
-          {
-            auth.loggedIn
-              ? (
-                <Box>
-                  <IconButton onClick={handleOpenMenu} ref={anchorRef}>
-                    <Avatar src="/static/images/lady-of-the-night.jpg" />
-                  </IconButton>
-                  <Menu
-                    open={menuOpen}
-                    onClose={handleCloseMenu}
-                    anchorEl={anchorRef.current}
-                    // anchorOrigin - Mygtuko(enchorEl) atspirties taškas
-                    anchorOrigin={{
-                      horizontal: 'right',
-                      vertical: 'bottom',
-                    }}
-                    // transformOrigin - <Menu> komponento turinio atspirties taškas
-                    transformOrigin={{
-                      horizontal: 'right',
-                      vertical: 'top',
-                    }}
-                  >
-                    <MenuItem onClick={handleCloseMenu}>
-                      <AuthMenuLink to={ProfileRoute.link}>
-                        <PersonIcon sx={{ mr: 2 }} />
-                        <Typography textAlign="center">Profile</Typography>
-                      </AuthMenuLink>
-                    </MenuItem>
-                    <Divider sx={{ my: 0.5 }} />
-                    <MenuItem onClick={handleLogout}>
-                      <PowerSettingsNewIcon sx={{ mr: 2 }} />
-                      <Typography textAlign="center">Logout</Typography>
-                    </MenuItem>
-                  </Menu>
-                </Box>
-              )
-              : null
-          }
+          {auth.loggedIn ? <AuthMenu /> : null}
         </Box>
       </StyledContainer>
     </AppBar>
