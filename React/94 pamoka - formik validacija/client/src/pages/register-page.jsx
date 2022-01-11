de2d670c55e45ac7e514467e8f0a716072e7215b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-11 08:34:38 +0200

    feat(94): Formik field values by name attribute

--- React/94 pamoka - formik validacija/client/src/pages/register-page.jsx
+++ React/94 pamoka - formik validacija/client/src/pages/register-page.jsx
@@ -4,70 +4,84 @@ import {
   FormControlLabel,
   Checkbox,
   Grid,
-  Box,
 } from '@mui/material';
+import { Formik } from 'formik';
 import AuthForm from '../components/auth-form';
 
-const title = ['Registruotis'];
-
 const RegisterPage = () => (
-  <AuthForm title={title} linkTo="/register" linkTitle="Jau turite paskyrą? Prisijunkite">
-    <Box>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            autoComplete="fname"
-            name="firstName"
-            variant="outlined"
-            required
-            fullWidth
-            id="firstName"
-            label="Vardas"
-            autoFocus
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            variant="outlined"
-            required
-            fullWidth
-            id="lastName"
-            label="Pavardė"
-            name="lastName"
-            autoComplete="lname"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            variant="outlined"
-            required
-            fullWidth
-            id="email"
-            label="El. paštas"
-            name="email"
-            autoComplete="email"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            variant="outlined"
-            required
-            fullWidth
-            name="password"
-            label="Slaptažodis"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-        </Grid>
-        <Grid item sx={{ mb: 2 }} xs={12}>
-          <FormControlLabel
-            control={<Checkbox value="allowExtraEmails" color="primary" />}
-            label="Noriu gauti su rinkodara susijusius pranešimus"
-          />
-        </Grid>
-      </Grid>
-    </Box>
-  </AuthForm>
+  <Formik
+    initialValues={{
+      name: '',
+      surname: '',
+      email: '',
+      password: '',
+      subscribe: false,
+    }}
+  >
+    {(formik) => {
+      const { handleChange } = formik;
+      return (
+        <AuthForm
+          title="Registruotis"
+          linkTo="/register"
+          linkTitle="Jau turite paskyrą? Prisijunkite"
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="name"
+                label="Vardas"
+                onChange={handleChange}
+                fullWidth
+                variant="outlined"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="surname"
+                label="Pavardė"
+                onChange={handleChange}
+                fullWidth
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="email"
+                label="El. paštas"
+                onChange={handleChange}
+                fullWidth
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="password"
+                label="Slaptažodis"
+                onChange={handleChange}
+                fullWidth
+                variant="outlined"
+                type="password"
+              />
+            </Grid>
+            <Grid item sx={{ mb: 2 }} xs={12}>
+              <FormControlLabel
+                control={(
+                  <Checkbox
+                    name="subscribe"
+                    onChange={handleChange}
+                    color="primary"
+                  />
+                )}
+                label="Noriu gauti su rinkodara susijusius pranešimus"
+              />
+            </Grid>
+          </Grid>
+        </AuthForm>
+      );
+    }}
+
+  </Formik>
 );
 export default RegisterPage;
