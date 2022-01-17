e6f3b7f0ba5f7de5f09780d23b8802bb2739ffb4
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-17 11:58:23 +0200

    feat(98): delete, put and patch handlers

--- Node/98 pamoka - Express.js įvadas/src/routes/fruit-router.js
+++ Node/98 pamoka - Express.js įvadas/src/routes/fruit-router.js
@@ -34,7 +34,51 @@ router.get('/:id', (req, res) => {
 });
 
 // DELETE '/fruits/:id' -> ištrinti vieną vaisių
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const ii = fruits.findIndex(x => x.id === id);
+  if (ii >= 0) {
+    const [deletedFruit] = fruits.splice(ii, 1);
+    res.status(200).json(deletedFruit);
+  } else {
+    res.status(404).json({
+      message: 'Vaisus nerastas'
+    })
+  }
+});
+
 // PATCH  '/fruits/:id' -> ATNAUJINTI vieną vaisių
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const fruit = fruits.find(x => x.id === id);
+  if (fruit) {
+    if (name) fruit.name = name;
+    if (price) fruit.price = price;
+    res.status(200).json(fruit);
+  }
+  else {
+    res.status(404).json({ message: 'Vaisus nerastas' });
+  }
+});
+
 // PUT    '/fruits/:id' -> Perrašo vieną vaisių
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const fruit = fruits.find(x => x.id === id);
+  if (fruit) {
+    if (name && price) {
+      fruit.name = name;
+      fruit.price = price;
+      res.status(200).json(fruit);
+    } else {
+      res.status(400).json({ message: 'Nepakanka duomenų' });
+    }
+  }
+  else {
+    res.status(404).json({ message: 'Vaisus nerastas' });
+  }
+});
 
 module.exports = router;
