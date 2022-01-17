33267649264b55666223174c123493b4dd8ca460
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-17 11:18:05 +0200

    feat(98): implemented GET /  routeHandler implemented GET /:id  routeHandler implemented POST /:id  routeHandler tasks for pratice

--- Node/98 pamoka - Express.js įvadas/src/routes/fruit-router.js
+++ Node/98 pamoka - Express.js įvadas/src/routes/fruit-router.js
@@ -1,21 +1,40 @@
 const express = require('express');
+const { v4: generateId } = require('uuid');
 
 const router = express.Router();
 
+const fruits = [
+  { id: '1', name: 'Apple', price: 20.89 },
+  { id: '2', name: 'Pear', price: 28.19 },
+  { id: '3', name: 'Banana', price: 12.99 },
+];
+
+// REST API standard
+
+// GET    '/fruits'     -> visus vaisius
 router.get('/', (req, res) => {
-  res.status(200).json({
-    products: [
-      { id: '1', name: 'Apple', price: 20.89 },
-      { id: '2', name: 'Pear', price: 28.19 },
-      { id: '3', name: 'Banana', price: 12.99 },
-    ]
-  })
+  res.status(200).json({ fruits })
 });
 
+// POST   '/fruits/'    -> sukurti vieną vaisių
 router.post('/', (req, res) => {
-  // Šioje vietoje įdėsime vaisių į duomenų bazę.
-  console.log(req.body);
+  const { name, price } = req.body;
+  fruits.push({
+    id: generateId(),
+    name,
+    price
+  })
   res.send('Vaisius sėkmingas įdėtas į prekybą');
 });
+
+// GET    '/fruits/:id' -> gauti vieną vaisių
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json(fruits.find(x => x.id === id));
+});
+
+// DELETE '/fruits/:id' -> ištrinti vieną vaisių
+// PATCH  '/fruits/:id' -> ATNAUJINTI vieną vaisių
+// PUT    '/fruits/:id' -> Perrašo vieną vaisių
 
 module.exports = router;
--- Node/98 pamoka - Express.js įvadas/src/server.js
+++ Node/98 pamoka - Express.js įvadas/src/server.js
@@ -17,12 +17,3 @@ server.use('/api/fruits', fruitRouter);
 server.listen(SERVER_PORT, () => {
   console.log(`puslapis veikia ant http://localhost:${SERVER_PORT}/`);
 });
-
-
-// REST API standard
-// GET    '/fruits'     -> visus vaisius
-// POST   '/fruits/'    -> sukurti vieną vaisių
-// GET    '/fruits/:id' -> gauti vieną vaisių
-// DELETE '/fruits/:id' -> ištrinti vieną vaisių
-// PATCH  '/fruits/:id' -> ATNAUJINTI vieną vaisių
-// PUT    '/fruits/:id' -> Perrašo vieną vaisių
