1a22ac0dae4a2b300d6d721801687d5552b5d718
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-20 08:38:45 +0200

    task(101): fetch users from database

--- Node/101 pamoka - authentication and authorization/src/controllers/user-controller.js
+++ Node/101 pamoka - authentication and authorization/src/controllers/user-controller.js
@@ -1,3 +1,9 @@
+/*
+  task: fetch users from database
+  įgalinkite visų User modelio dokumentų parsiuntimą
+  getUsers requestHandler'io metu.
+*/
+
 const getUsers = async (req, res) => {
   res.status(200).json({
     users: []
