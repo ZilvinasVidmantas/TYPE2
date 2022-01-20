b39a6b7a37db9cecc26ce9e302b9a1160c5375a2
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-20 10:11:27 +0200

    feat(101): authorization middlewares

--- Node/101 pamoka - authentication and authorization/src/controllers/user-controller.js
+++ Node/101 pamoka - authentication and authorization/src/controllers/user-controller.js
@@ -3,7 +3,7 @@ const UserViewModel = require('../view-models/user-view-model');
 
 const getUsers = async (req, res) => {
   const userDocs = await UserModel.find();
-  const users = userDocs.map(userDoc => new UserViewModel(userDoc))
+  const users = userDocs.map(userDoc => new UserViewModel(userDoc));
   res.status(200).json({ users });
 };
 
--- Node/101 pamoka - authentication and authorization/src/routes/user-router.js
+++ Node/101 pamoka - authentication and authorization/src/routes/user-router.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const authMiddleware = require('../middlewares/auth-middleware');
+const adminMiddleware = require('../middlewares/admin-middleware');
 const {
   getUsers
 } = require('../controllers/user-controller');
 
 const router = express.Router();
 
-router.get('/', getUsers);
+// middlewares
+router.use(authMiddleware);
+
+router.get('/', adminMiddleware, getUsers);
 
 module.exports = router;
 
