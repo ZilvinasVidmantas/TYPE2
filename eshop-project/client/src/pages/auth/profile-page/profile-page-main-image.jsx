bc949ac2da55efd8408c10320288dcba52048d5a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-01-28 08:32:40 +0200

    task(107): useState task

--- eshop-project/client/src/pages/auth/profile-page/profile-page-main-image.jsx
+++ eshop-project/client/src/pages/auth/profile-page/profile-page-main-image.jsx
@@ -12,6 +12,24 @@ const StyledMainImage = styled('img')({
   objectFit: 'cover',
 });
 
+/*
+  Paspaudus mygtuką "Keisti", šio komponento <Box> fonas pasidaro raudonas
+  Paspaudus dar kartą raudonas fonas dingsta:
+    paspaudimas 1 - raudona
+    paspaudimas 2 - fono nėra
+    paspaudimas 3 - raudona
+    paspaudimas 4 - fono nėra
+    paspaudimas 5 - raudona
+
+  Tai padarykite sukurdami būsenos kintamajį
+    * paspaudus mygtuką, keičiamas kintamasis
+    * fonas priklauso nuo šio būsenos kintamojo reikšmės
+
+  * Ar persikrauna komponentas paspaudusu mygtuką?
+  * Kokiais atvejais persikrauna komponentas?
+  * Ar vykdosi useState funkcija, kiekvieną kartą persikrovus komponentui?
+*/
+
 const MainImage = ({ mainImg }) => (
   <Box sx={{
     display: { xs: 'flex' },
