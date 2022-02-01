7fbb131e1382fa5f724a8b020665bc67c51695ba
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-01 13:52:40 +0200

    feat(109): exercise answers

--- TypeScript [108-123]/109 pamoka - Tipų aprašymas/pratimai/atsakymai/TS-2-exercises/public/js/main.js
+++ TypeScript [108-123]/109 pamoka - Tipų aprašymas/pratimai/atsakymai/TS-2-exercises/public/js/main.js
@@ -1,34 +1,36 @@
 "use strict";
-console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
+console.group('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
 {
+    const sortDrinksByPriceASC = (drink1, drink2) => {
+        return drink1.price - drink2.price;
+    };
+    const solution = (drinks) => {
+        return [...drinks].sort(sortDrinksByPriceASC);
+    };
+    const drinks = [
+        { name: "lemonade", price: 50 },
+        { name: "fanta", price: 30 },
+        { name: "lime", price: 10 },
+    ];
+    const sortedDrinks = solution(drinks);
+    console.log({
+        drinks,
+        sortedDrinks,
+    });
 }
 console.groupEnd();
-console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
+console.group('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
 {
-}
-console.groupEnd();
-console.groupCollapsed('3 - https://edabit.com/challenge/48EJWLhF224na8po3');
-{
-}
-console.groupEnd();
-console.groupCollapsed('4 - https://edabit.com/challenge/i6YqzHcSiPiEQKjeX');
-{
-}
-console.groupEnd();
-console.groupCollapsed('5 - https://edabit.com/challenge/8s2jy9hR2TAeQinKD');
-{
-}
-console.groupEnd();
-console.groupCollapsed('6 - https://edabit.com/challenge/pPNAs5PvB3WvnDwDM');
-{
-}
-console.groupEnd();
-console.groupCollapsed('7 - https://edabit.com/challenge/QXWM2oo7rQNiyDsip');
-{
-}
-console.groupEnd();
-console.groupCollapsed('8 - https://edabit.com/challenge/pLNavsePxJ87t9Nak');
-{
+    const solution = (itemObj, itemName, itemValue) => (Object.assign(Object.assign({}, itemObj), { [itemName]: itemValue }));
+    const solutionArgs1 = [{}, "Brutus", 300];
+    const solutionArgs2 = [{ piano: 500 }, "Brutus", 400];
+    const solutionArgs3 = [{ piano: 500, stereo: 300 }, "Caligula", 440];
+    const result1 = solution(...solutionArgs1);
+    const result2 = solution(...solutionArgs2);
+    const result3 = solution(...solutionArgs3);
+    console.log(solutionArgs1, result1);
+    console.log(solutionArgs2, result2);
+    console.log(solutionArgs3, result3);
 }
 console.groupEnd();
 //# sourceMappingURL=main.js.map
--- TypeScript [108-123]/109 pamoka - Tipų aprašymas/pratimai/atsakymai/TS-2-exercises/src/main.ts
+++ TypeScript [108-123]/109 pamoka - Tipų aprašymas/pratimai/atsakymai/TS-2-exercises/src/main.ts
@@ -1,47 +1,56 @@
-console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
+console.group('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
 {
+  type Drink = {
+    name: string,
+    price: number,
+  };
 
+  const sortDrinksByPriceASC = (drink1: Drink, drink2: Drink): number => {
+    return drink1.price - drink2.price;
+  }
+
+  const solution = (drinks: Array<Drink>): Array<Drink> => {
+    return [...drinks].sort(sortDrinksByPriceASC);
+  };
+
+  const drinks: Drink[] = [
+    { name: "lemonade", price: 50 },
+    { name: "fanta", price: 30 },
+    { name: "lime", price: 10 },
+  ];
+
+  const sortedDrinks: Drink[] = solution(drinks);
+
+  console.log({
+    drinks,
+    sortedDrinks,
+  });
 }
 console.groupEnd();
 
-console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
+console.group('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
 {
+  type ItemObj = {
+    [key: string]: number,
+  };
 
-}
-console.groupEnd();
+  const solution = (itemObj: ItemObj, itemName: string, itemValue: number): ItemObj => ({
+    ...itemObj,
+    [itemName]: itemValue,
+  });
 
-console.groupCollapsed('3 - https://edabit.com/challenge/48EJWLhF224na8po3');
-{
+  type SolutionArgs = [ItemObj, string, number];
 
-}
-console.groupEnd();
+  const solutionArgs1: SolutionArgs = [{}, "Brutus", 300];
+  const solutionArgs2: SolutionArgs = [{ piano: 500 }, "Brutus", 400];
+  const solutionArgs3: SolutionArgs = [{ piano: 500, stereo: 300 }, "Caligula", 440];
 
-console.groupCollapsed('4 - https://edabit.com/challenge/i6YqzHcSiPiEQKjeX');
-{
+  const result1: ItemObj = solution(...solutionArgs1);
+  const result2: ItemObj = solution(...solutionArgs2);
+  const result3: ItemObj = solution(...solutionArgs3);
 
-}
-console.groupEnd();
-
-console.groupCollapsed('5 - https://edabit.com/challenge/8s2jy9hR2TAeQinKD');
-{
-
-}
-console.groupEnd();
-
-console.groupCollapsed('6 - https://edabit.com/challenge/pPNAs5PvB3WvnDwDM');
-{
-
-}
-console.groupEnd();
-
-console.groupCollapsed('7 - https://edabit.com/challenge/QXWM2oo7rQNiyDsip');
-{
-
-}
-console.groupEnd();
-
-console.groupCollapsed('8 - https://edabit.com/challenge/pLNavsePxJ87t9Nak');
-{
-
+  console.log(solutionArgs1, result1);
+  console.log(solutionArgs2, result2);
+  console.log(solutionArgs3, result3);
 }
 console.groupEnd();
