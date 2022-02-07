8e7e561fbba2313cc23f67bb78bd5c15e489ea75
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-07 08:03:17 +0200

    init(113): Generics

---
+++ TypeScript [108-123]/113 pamoka - Bendriniai  tipai/pratimai/atsakymai/public/js/main.js
@@ -0,0 +1,109 @@
+"use strict";
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const strings = ["pirmadienis", "antradienis", "trečiadienis", "ketvirtadienis", "penktadienis", "šeštadienis", "sekmadienis"];
+const booleans = [true, true, true, true, false];
+console.group('1. Parašykite funkciją, kuri grąžina pirmą masyvo elementą.');
+{
+    function solution(arr) {
+        return arr[0];
+    }
+    console.log({ numbers, result: solution(numbers) });
+    console.log({ strings, result: solution(strings) });
+    console.log({ booleans, result: solution(booleans) });
+}
+console.groupEnd();
+console.group('2. Parašykite funkciją, kuri grąžina paskutinį masyvo elementą.');
+{
+    function solution(arr) {
+        return arr[arr.length - 1];
+    }
+    console.log({ numbers, result: solution(numbers) });
+    console.log({ strings, result: solution(strings) });
+    console.log({ booleans, result: solution(booleans) });
+}
+console.groupEnd();
+console.group('3. Parašykite funkciją, kuri grąžina vienarūšių primityvių reikšmių masyvo kopiją');
+{
+    function solution(arr) {
+        const copy = arr.map(x => x);
+        return copy;
+    }
+    console.log({ numbers, result: solution(numbers) });
+    console.log({ strings, result: solution(strings) });
+    console.log({ booleans, result: solution(booleans) });
+}
+console.groupEnd();
+console.group('4. Parašykite funkciją,  kuri pirmu parametru priima string | number | boolen, grąžina to tipo masyvą su perduota reikšme tiek kartų, kiek nurodyta antru parametru');
+{
+    function solution(value, count) {
+        return Array.from(new Array(count)).map(_ => value);
+    }
+    const dataSamples = [
+        ['a', 2],
+        [77, 4],
+        [true, 1],
+    ];
+    dataSamples.forEach((args) => console.log(`solution(${args.join(', ')}):`, solution(...args)));
+}
+console.groupEnd();
+console.group('5. Parašykite funkciją, kuri sujungia tokių pat tipų masyvus į vieną masyvą');
+{
+    function solution(arr1, arr2) {
+        return [...arr1, ...arr2];
+    }
+    const args1 = [[1, 2, 3], [4, 5, 6]];
+    const args2 = [['labas', 'mano', 'vardas'], ['yra', 'ponas', 'krabas']];
+    const args3 = [[true, true, true], [false, false, false]];
+    console.log({ args: args1, result: solution(...args1) });
+    console.log({ args: args2, result: solution(...args2) });
+    console.log({ args: args3, result: solution(...args3) });
+}
+console.groupEnd();
+console.group('6. Parašykite funkciją, kuri priimtų bet kokią reikšmę ir grąžintų objektą su savybėmis-funkcijomis "setValue" - reikšmei nustatyti ir "getValue" tai reikšmei nustatyti. Funkcijai perduota reikšmė neturi būti pasiekiama tiesiogiai.');
+{
+    function solution(initialValue) {
+        let value = initialValue;
+        return {
+            setValue: (newValue) => value = newValue,
+            getValue: () => value,
+        };
+    }
+    const value1 = 7;
+    const value2 = ["Sidnius", "Mauricijus", "Penktasis"];
+    const value3 = { name: 'Fanatijus', surname: 'Labdara' };
+    const obj1 = solution(value1);
+    const obj2 = solution(value2);
+    const obj3 = solution(value3);
+    console.log('initial values');
+    console.log({
+        value1: obj1.getValue(),
+        value2: obj2.getValue(),
+        value3: obj3.getValue(),
+    });
+    console.log('changing values...');
+    obj1.setValue(9);
+    obj2.setValue(['Pakeista']);
+    obj3.setValue({ name: 'Pakaitalas', surname: 'Fuflo' });
+}
+console.groupEnd();
+console.group('6. Perskaitykite straipsnį apie dvipusio susieto sąrašo struktūrą(Doubly Linked List) ir parašykite implementaciją naudojant TypeScript.');
+{
+    console.group('6.1 Sukurkite funkciją iteruoti per dvipusį sąrašą.');
+    {
+    }
+    console.groupEnd();
+    console.group('6.2 Sukurkite funkciją iteruoti per dvipusį sąrašą atvirkščia tvarka.');
+    {
+    }
+    console.groupEnd();
+    console.group('6.3 Sukurkite funkciją paversti dvipusį sąrašą masyvu');
+    {
+    }
+    console.groupEnd();
+    console.group('6.4 Sukurkite funkciją paversti masyvą dvipusiu sąrašu');
+    {
+    }
+    console.groupEnd();
+}
+console.groupEnd();
+//# sourceMappingURL=main.js.map
---
+++ TypeScript [108-123]/113 pamoka - Bendriniai  tipai/pratimai/uzduotys/src/main.ts
@@ -0,0 +1,40 @@
+type PrimitiveType = string | number | boolean;
+
+console.group('1. Parašykite funkciją, kuri grąžina pirmą masyvo elementą.');
+{
+  // Sprendimas ir rezultatų spausdinimas
+}
+console.groupEnd();
+
+console.group('2. Parašykite funkciją, kuri grąžina paskutinį masyvo elementą.');
+{
+  // Sprendimas ir rezultatų spausdinimas
+}
+console.groupEnd();
+
+console.group('3. Parašykite funkciją, kuri grąžina vienarūšių primityvių reikšmių masyvo kopiją');
+{
+  // Sprendimas ir rezultatų spausdinimas
+}
+console.groupEnd();
+
+console.group('4. Parašykite funkciją,  kuri pirmu parametru priima string | number | boolen, grąžina to tipo masyvą su perduota reikšme tiek kartų, kiek nurodyta antru parametru');
+{
+  // ('a', 2) -> ['a', 'a']
+  // (77, 4) -> [77, 77, 77, 77]
+  // (true, 1) -> [true]
+  // Sprendimas ir rezultatų spausdinimas
+}
+console.groupEnd();
+
+console.group('5. Parašykite funkciją, kuri sujungia tokių pat tipų masyvus į vieną masyvą');
+{
+  // Sprendimas ir rezultatų spausdinimas
+}
+console.groupEnd();
+
+console.group('6. Parašykite funkciją, kuri priimtų bet kokią reikšmę ir grąžintų objektą su savybėmis-funkcijomis "setValue" - reikšmei nustatyti ir "getValue" tai reikšmei nustatyti. Funkcijai perduota reikšmė neturi būti pasiekiama tiesiogiai.');
+{
+  // Sprendimas ir rezultatų spausdinimas
+}
+console.groupEnd();
