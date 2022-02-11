699854695451d691018d056447622185df920caf
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-11 09:35:03 +0200

    feat(117): 2nd and 3rd task

--- TypeScript [108-123]/117 pamoka - klasės/užduotys/src/main.ts
+++ TypeScript [108-123]/117 pamoka - klasės/užduotys/src/main.ts
@@ -6,7 +6,19 @@ class Person {
   }
 }
 
-console.log('1. Sukurkite Person klasei, savybes "name" ir "surname". Kiekvienai iš jų sukurkite setterius, ir bendrą getterį fullname');
+console.log('1. Sukurkite Person klasei savybes "name" ir "surname". Kiekvienai iš jų sukurkite setterius, ir bendrą getterį fullname');
+{
+
+}
+console.groupEnd();
+
+console.log('2. Sukurkite Person klasei savybę age. Inkapsuliuokite šią savybę taip, jog reikšmė galėtų būti tik sveiki skaičiai nuo 1 iki 150');
+{
+
+}
+console.groupEnd();
+
+console.log('3. Sukurkite Person klasei savybę height kurios vertė būtų saugoma centimetrais. Sukurkite šiai savybei setterį, kuris pirmu parametru priimtų reikšmę, o antru parametru priimtu matavimo vienetus: "cm" | "m" | "in". Jeigu antras parametras nėra perduotas, numatytas matavimo vienetas turi būti cm. Getteris turi grąžinti reikšmę centimetrais');
 {
 
 }
