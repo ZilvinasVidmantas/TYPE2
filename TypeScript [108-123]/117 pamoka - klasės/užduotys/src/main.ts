d54d6ec31c9db7fadf3b2a14d0be4ece7e68147a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-11 09:28:04 +0200

    init(117): classes

---
+++ TypeScript [108-123]/117 pamoka - klasės/užduotys/src/main.ts
@@ -0,0 +1,13 @@
+class Person {
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+console.log('1. Sukurkite Person klasei, savybes "name" ir "surname". Kiekvienai iš jų sukurkite setterius, ir bendrą getterį fullname');
+{
+
+}
+console.groupEnd();
