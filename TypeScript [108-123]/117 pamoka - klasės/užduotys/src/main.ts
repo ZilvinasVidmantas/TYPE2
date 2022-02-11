8edab801b0075a89e8dc03bf063891d676b82469
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-11 09:51:11 +0200

    feat(117): Incapsulation tasks

--- TypeScript [108-123]/117 pamoka - klasės/užduotys/src/main.ts
+++ TypeScript [108-123]/117 pamoka - klasės/užduotys/src/main.ts
@@ -12,14 +12,40 @@ console.log('1. Sukurkite Person klasei savybes "name" ir "surname". Kiekvienai 
 }
 console.groupEnd();
 
-console.log('2. Sukurkite Person klasei savybę age. Inkapsuliuokite šią savybę taip, jog reikšmė galėtų būti tik sveiki skaičiai nuo 1 iki 150');
+console.log('2. Sukurkite Person klasei savybę "ag"e. Inkapsuliuokite šią savybę taip, jog reikšmė galėtų būti tik sveiki skaičiai nuo 1 iki 150');
 {
 
 }
 console.groupEnd();
 
-console.log('3. Sukurkite Person klasei savybę height kurios vertė būtų saugoma centimetrais. Sukurkite šiai savybei setterį, kuris pirmu parametru priimtų reikšmę, o antru parametru priimtu matavimo vienetus: "cm" | "m" | "in". Jeigu antras parametras nėra perduotas, numatytas matavimo vienetas turi būti cm. Getteris turi grąžinti reikšmę centimetrais');
+console.log('3. Sukurkite Person klasei savybę "height" kurios vertė būtų saugoma centimetrais. Sukurkite šiai savybei setterį, kuris pirmu parametru priimtų reikšmę, o antru parametru priimtų matavimo vienetus: "cm" | "m" | "in". Jeigu antras parametras nėra perduotas, numatytas(default) matavimo vienetas turi būti cm. Getteris turi grąžinti reikšmę centimetrais.');
 {
 
 }
 console.groupEnd();
+
+console.log('4. Sukurkite Person klasei statinę savybę "heightUnits". Jos tipas turi būti išvardinimas(enum), kurio pasirinkimai yra: "CM", "M", "IN". Numatytoji(default) "heightUnits" reikšmė turi būti centimetrai');
+{
+
+}
+console.groupEnd();
+
+console.log('5. "height" setterio antram parametrui pakeiskite sąjungos tipą į [4.] užduotyje sukurtą išvardinimą(enum). Priderinkite pavyzdžius ir metodą.');
+{
+
+}
+console.groupEnd();
+
+console.log('6. "height" geteriui sukurkite logiką, jog jis grąžintų matavimo vienetus, pagal statinės savybės "heightUnits" reikšmę.');
+{
+
+}
+console.groupEnd();
+
+console.log('7. Analogiškai pagal [4.]-[6.] punktus sukurkite savybę weight su statiniu išvardinimu "weightUnits", kurio pasirinkimai turi būti: "KG", "LBS"');
+{
+
+}
+console.groupEnd();
+
+console.log('8. Sukurkite klasei Person metodą "toString". Kuris paverstų žmogaus savybes gražiu formatu: vardas ir pavardė pirmoje eilutėje, o "height" ir "weight" savybės atskirose eilutėse, atitrauktos nuo kairio krašto per "tab" simbolį, ir su matavimo vienetais(kurie išsaugoti) statinėse Person klasės savybėse');
