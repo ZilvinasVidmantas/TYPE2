1990b4d8fdbfecef17d84ec399da3d227a7da07b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-17 08:57:11 +0200

    feat(120): webpack minor example

---
+++ TypeScript [108-123]/120 pamoka - typeScript konfigūracija su react 1/webpack-example/src/index.js
@@ -0,0 +1,13 @@
+import './styles/main.scss';
+
+const a = (c, d) => {
+  return c + d
+}
+
+const arr1 = [1, 2, 3];
+const arr2 = [3, 2, 1]
+const arr3 = [...arr1, ...arr2];
+
+console.log(a(1, 3))
+console.log(arr3);
+console.log(arr3);
