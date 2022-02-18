4937e6a9dbc37627554f815cb32a957c5a9b1ddc
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-18 11:30:49 +0200

    refactor(121): styled type simplified

--- TypeScript [108-123]/121 pamoka - typeScript intergracija su Material-ui/src/pages/posts-page/card/card-container.tsx
+++ TypeScript [108-123]/121 pamoka - typeScript intergracija su Material-ui/src/pages/posts-page/card/card-container.tsx
@@ -1,19 +1,16 @@
 import {
   Paper,
   styled,
-  PaperProps,
 } from '@mui/material';
 import { PaletteColorNames } from '../../../types/theme-types';
 
-type CardContainerProps = PaperProps & { bgcolor?: PaletteColorNames };
+type AdditionalProps = { bgcolor?: PaletteColorNames };
 
-type CardContainerType = React.FC<CardContainerProps>;
-
-const CardContainer = styled(Paper)<CardContainerProps>(({ theme, bgcolor }) => ({
+const CardContainer = styled(Paper)<AdditionalProps>(({ theme, bgcolor }) => ({
   width: 200,
   borderRadius: 4,
   display: 'inline-block',
   backgroundColor: bgcolor && theme.palette[bgcolor].main,
-})) as CardContainerType;
+}));
 
 export default CardContainer;
