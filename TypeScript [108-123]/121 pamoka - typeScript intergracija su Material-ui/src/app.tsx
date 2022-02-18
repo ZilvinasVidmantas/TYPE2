7b80ce7e772829a0d24f648f8a130cb730ae07d2
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-18 13:32:10 +0200

    feat(121): theme tasks

--- TypeScript [108-123]/121 pamoka - typeScript intergracija su Material-ui/src/app.tsx
+++ TypeScript [108-123]/121 pamoka - typeScript intergracija su Material-ui/src/app.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import {
   Routes,
@@ -9,21 +9,25 @@ import BaseLayout from './layouts/base-layout';
 import HomePage from './pages/home-page';
 import PostsPage from './pages/posts-page';
 import UsersPage from './pages/users-page';
+import theme from './styles/theme';
 
 const App = () => {
 
   return (
-    <CssBaseline>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<BaseLayout />} >
-            <Route index element={<HomePage />} />
-            <Route path="posts" element={<PostsPage />} />
-            <Route path="users" element={<UsersPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </CssBaseline>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<BaseLayout />} >
+              <Route index element={<HomePage />} />
+              <Route path="posts" element={<PostsPage />} />
+              <Route path="users" element={<UsersPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CssBaseline>
+    </ThemeProvider>
+
   );
 };
 
