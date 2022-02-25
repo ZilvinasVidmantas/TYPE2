d33b3843333f1ae2fcae8e85888e9772fded6991
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-25 10:50:57 +0200

    feat(126): theme changes

--- eshop-project [102-132]/client/src/theme/index.tsx
+++ eshop-project [102-132]/client/src/theme/index.tsx
@@ -1,6 +1,9 @@
 import { createTheme } from '@mui/material';
 
 const theme = createTheme({
+  shape: {
+    borderRadius: 0
+  },
   mixins: {
     navbar: {
       height: 56,
@@ -11,6 +14,26 @@ const theme = createTheme({
       slow: 600,
     },
   },
+  palette: {
+    primary: {
+      light: '#555555',
+      main: '#454545',
+      dark: '#353535',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#06d1ad',
+      main: '#08bf9e',
+      dark: '#03a387',
+      contrastText: '#fff',
+    },
+    background: {
+      default: '#fafafa'
+    }
+  },
+  typography: {
+    fontFamily: '\'Mulish\', sans-serif'
+  }
 });
 
 export default theme;
