5ed2f82485cff752b65dbf8c6418ad40bb76ee6b
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-02-25 09:33:22 +0200

    feat(126): Range filter view

--- eshop-project [102-132]/client/src/pages/home-page/home-page-filters/range-filter.tsx
+++ eshop-project [102-132]/client/src/pages/home-page/home-page-filters/range-filter.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import {
+  Slider,
+  Typography,
+  Box,
+  Input,
+  styled,
+} from '@mui/material';
+
+const RangeFilterInput = styled(Input)({
+  width: 50,
+  input: {
+    textAlign: 'center'
+  },
+});
 
 const RangeFilter = () => {
   return (
-    <div>RangeFilter</div>
+    <Box>
+      <Typography variant="h6">Filtrio pavadinimas</Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
+        <RangeFilterInput value={10} />
+        <RangeFilterInput value={100} />
+      </Box>
+      <Box sx={{ px: 2, pt: 2 }}>
+        <Slider
+          min={10}
+          max={100}
+          value={[10, 100]}
+          onChange={() => console.log('Keiciamas Slider\'is')}
+        />
+      </Box>
+    </Box>
   );
 };
 
